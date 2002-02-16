5bf62a8b01109c9cb3106af5504b696dc29df031
ingokegel <ingo.kegel@ej-technologies.com>
2002-02-16 10:14:51 +0000

    huge read/write performance optimization removed setBytes()

--- jclasslib/src/org/gjt/jclasslib/structures/constants/ConstantUtf8Info.java
+++ jclasslib/src/org/gjt/jclasslib/structures/constants/ConstantUtf8Info.java
@@ -13,15 +13,15 @@ import java.io.*;
 
 /**
     Describes a <tt>CONSTANT_Utf8_info</tt> constant pool data structure.
- 
+
     @author <a href="mailto:jclasslib@gmx.net">Ingo Kegel</a>
-    @version $Revision: 1.1.1.1 $ $Date: 2001-05-14 16:49:19 $
+    @version $Revision: 1.2 $ $Date: 2002-02-16 10:14:51 $
 */
 public class ConstantUtf8Info extends CPInfo {
 
     private byte[] bytes;
     private String string;
-    
+
     public byte getTag() {
         return CONSTANT_UTF8;
     }
@@ -29,7 +29,7 @@ public class ConstantUtf8Info extends CPInfo {
     public String getTagVerbose() {
         return CONSTANT_UTF8_VERBOSE;
     }
-    
+
     public String getVerbose() throws InvalidByteCodeException {
         return string;
     }
@@ -39,15 +39,7 @@ public class ConstantUtf8Info extends CPInfo {
         @return the array
      */
     public byte[] getBytes() {
-        return bytes;
-    }
-    
-    /**
-        Set the byte array of the string in this entry.
-        @param bytes the array
-     */
-    public void setBytes(byte[] bytes) {
-        this.bytes = bytes;
+        return string.getBytes();
     }
 
     /**
@@ -60,20 +52,17 @@ public class ConstantUtf8Info extends CPInfo {
 
     public void read(DataInput in)
         throws InvalidByteCodeException, IOException {
-            
-        int length = in.readUnsignedShort();
-        bytes = new byte[length];
-        in.readFully(bytes);
-        string = new String(bytes, "UTF-8");
-        if (debug) debug("read ");
+
+        string = in.readUTF();
+
+		if (debug) debug("read ");
     }
-    
+
     public void write(DataOutput out)
         throws InvalidByteCodeException, IOException {
 
         out.writeByte(CONSTANT_UTF8);
-        out.writeShort(getLength(bytes));
-        out.write(bytes);
+        out.writeUTF(string);
         if (debug) debug("wrote ");
     }
 
