367354f5f2d5910ef352cd9e36243a370c40ce5d
Ingo Kegel <ingo.kegel@ej-technologies.com>
2014-05-19 13:43:53 +0200

    incomplete type annotations

---
+++ src/main/java/org/gjt/jclasslib/browser/detail/TypeAnnotationDetailPane.java
@@ -0,0 +1,59 @@
+/*
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public
+ License as published by the Free Software Foundation; either
+ version 2 of the license, or (at your option) any later version.
+ */
+
+/*
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the license, or (at your option) any later version.
+*/
+package org.gjt.jclasslib.browser.detail;
+
+import org.gjt.jclasslib.browser.BrowserServices;
+import org.gjt.jclasslib.browser.BrowserTreeNode;
+import org.gjt.jclasslib.structures.elementvalues.TypeAnnotationElementValue;
+import org.gjt.jclasslib.util.ExtendedJLabel;
+
+import javax.swing.tree.TreePath;
+
+/**
+ * Class for showing a type annotation node.
+ */
+public class TypeAnnotationDetailPane extends FixedListDetailPane {
+
+    private ExtendedJLabel lblType;
+    private ExtendedJLabel lblTypeVerbose;
+
+    private ExtendedJLabel lblInfo;
+
+    public TypeAnnotationDetailPane(BrowserServices services) {
+        super(services);
+    }
+
+    protected void setupLabels() {
+        addDetailPaneEntry(normalLabel("Target Type:"),
+            lblType = highlightLabel(),
+            lblTypeVerbose = highlightLabel());
+
+        addDetailPaneEntry(normalLabel("Target Info:"),
+            lblInfo = highlightLabel());
+
+    }
+
+    public void show(TreePath treePath) {
+        TypeAnnotationElementValue annotation = (TypeAnnotationElementValue)
+            ((BrowserTreeNode)treePath.getLastPathComponent()).getElement();
+
+        lblType.setText(annotation.getTargetType().toString());
+        lblTypeVerbose.setText("<" + annotation.getTargetType() + ">");
+
+        lblInfo.setText(annotation.getTargetInfo());
+
+
+        super.show(treePath);
+    }
+}
---
+++ src/main/java/org/gjt/jclasslib/structures/attributes/RuntimeTypeAnnotationsAttribute.java
@@ -0,0 +1,93 @@
+/*
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public
+ License as published by the Free Software Foundation; either
+ version 2 of the license, or (at your option) any later version.
+ */
+
+/*
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the license, or (at your option) any later version.
+*/
+package org.gjt.jclasslib.structures.attributes;
+
+import org.gjt.jclasslib.structures.AttributeInfo;
+import org.gjt.jclasslib.structures.InvalidByteCodeException;
+import org.gjt.jclasslib.structures.elementvalues.TypeAnnotationElementValue;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+/**
+ * Common class for runtime type annotations.
+ *
+ */
+public class RuntimeTypeAnnotationsAttribute extends AttributeInfo implements AnnotationHolder {
+    private static final int INITIAL_LENGTH = 2;
+
+    protected TypeAnnotationElementValue[] runtimeTypeAnnotations;
+
+
+    /**
+     * Get the list of runtime annotations associations of the parent
+     * structure as an array of <tt>Annotation</tt> structures.
+     *
+     * @return the array
+     */
+    public TypeAnnotationElementValue[] getRuntimeAnnotations() {
+        return runtimeTypeAnnotations;
+    }
+
+    /**
+     * Set the list of runtime annotations associations of the parent
+     * structure as an array of <tt>Annotation</tt> structures.
+     *
+     * @param runtimeAnnotations the array
+     */
+    public void setRuntimeAnnotations(TypeAnnotationElementValue[] runtimeAnnotations) {
+        this.runtimeTypeAnnotations = runtimeAnnotations;
+    }
+
+    public void read(DataInput in) throws InvalidByteCodeException, IOException {
+
+        int runtimeVisibleAnnotationsLength = in.readUnsignedShort();
+        runtimeTypeAnnotations = new TypeAnnotationElementValue[runtimeVisibleAnnotationsLength];
+        for (int i = 0; i < runtimeVisibleAnnotationsLength; i++) {
+            runtimeTypeAnnotations[i] = new TypeAnnotationElementValue();
+            runtimeTypeAnnotations[i].setClassFile(classFile);
+            runtimeTypeAnnotations[i].read(in);
+        }
+
+        if (debug) debug("read ");
+    }
+
+    public void write(DataOutput out)
+            throws InvalidByteCodeException, IOException {
+
+        super.write(out);
+
+        int runtimeVisibleAnnotationsLength = getLength(runtimeTypeAnnotations);
+
+        out.writeShort(runtimeVisibleAnnotationsLength);
+        for (int i = 0; i < runtimeVisibleAnnotationsLength; i++) {
+            runtimeTypeAnnotations[i].write(out);
+        }
+
+        if (debug) debug("wrote ");
+    }
+
+    public int getAttributeLength() {
+        int length = INITIAL_LENGTH;
+        for (TypeAnnotationElementValue runtimeTypeAnnotation : runtimeTypeAnnotations) {
+            length += runtimeTypeAnnotation.getLength();
+        }
+        return length;
+    }
+    
+	public int getNumberOfAnnotations() {
+		return runtimeTypeAnnotations.length;
+	}
+}
