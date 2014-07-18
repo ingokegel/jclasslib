33c8ff23c8a0c23c1da2b57f29a18fb5627a9ce7
Ingo Kegel <ingo.kegel@ej-technologies.com>
2014-07-18 17:05:14 +0200

    Regression: NOP opcodes broke the bytecode display

--- modules/data/src/main/java/org/gjt/jclasslib/bytecode/Opcode.java
+++ modules/data/src/main/java/org/gjt/jclasslib/bytecode/Opcode.java
@@ -229,7 +229,7 @@ public enum Opcode {
     private static final String JVM_SPEC_URL = "http://docs.oracle.com/javase/specs/jvms/se7/html/jvms-6.html#jvms-6.5.";
 
     public static Opcode getFromBytecode(int bytecode) {
-        if (bytecode < 256 && bytecode > 0) {
+        if (bytecode < 256 && bytecode >= 0) {
             return LOOKUP[bytecode];
         } else {
             return null;
