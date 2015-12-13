1d9ea4f171903e22b2b4cab382c4c3fc7f804a1b
ingo <ingo.kegel@ej-technologies.com>
2015-12-13 17:50:33 +0100

    PaddedInstruction j2k (conversion)

--- modules/data/src/main/java/org/gjt/jclasslib/bytecode/PaddedInstruction.kt
+++ modules/data/src/main/java/org/gjt/jclasslib/bytecode/PaddedInstruction.kt
@@ -5,60 +5,53 @@
     version 2 of the license, or (at your option) any later version.
 */
 
-package org.gjt.jclasslib.bytecode;
+package org.gjt.jclasslib.bytecode
 
-import org.gjt.jclasslib.io.ByteCodeInput;
-import org.gjt.jclasslib.io.ByteCodeOutput;
+import org.gjt.jclasslib.io.ByteCodeInput
+import org.gjt.jclasslib.io.ByteCodeOutput
 
-import java.io.IOException;
+import java.io.IOException
 
 /**
-    Base class for instructions which need a four byte padding relative
-    to the start of the enclosing code of the parent <tt>Code</tt>
-    attribute before reading immediate arguments.
- 
-    @author <a href="mailto:jclasslib@ej-technologies.com">Ingo Kegel</a>
-*/
-public class PaddedInstruction extends AbstractInstruction {
+ * Base class for instructions which need a four byte padding relative
+ * to the start of the enclosing code of the parent Code
+ * attribute before reading immediate arguments.
+
+ * @author [Ingo Kegel](mailto:jclasslib@ej-technologies.com)
+ */
+open class PaddedInstruction(opcode: Opcode) : AbstractInstruction(opcode) {
 
     /**
-        Constructor.
-        @param opcode the opcode.
+     * Get the padded size in bytes of this instruction.
+     * @param offset the offset at which this instruction is found.
+     * @return the padded size in bytes
      */
-    public PaddedInstruction(Opcode opcode) {
-        super(opcode); 
+    fun getPaddedSize(offset: Int): Int {
+        return size + paddingBytes(offset + 1)
     }
 
-    /**
-        Get the padded size in bytes of this instruction.
-        @param offset the offset at which this instruction is found.
-        @return the padded size in bytes
-     */
-    public int getPaddedSize(int offset) {
-        return getSize() + paddingBytes(offset + 1);
-    }
+    @Throws(IOException::class)
+    override fun read(input: ByteCodeInput) {
+        super.read(input)
 
-    public void read(ByteCodeInput in) throws IOException {
-        super.read(in);
-        
-        int bytesToRead = paddingBytes(in.getBytesRead());
-        for (int i = 0; i < bytesToRead; i++) {
-            in.readByte();
+        val bytesToRead = paddingBytes(input.bytesRead)
+        for (i in 0..bytesToRead - 1) {
+            input.readByte()
         }
     }
 
-    public void write(ByteCodeOutput out) throws IOException {
-        super.write(out);
-        
-        int bytesToWrite = paddingBytes(out.getBytesWritten());
-        for (int i = 0; i < bytesToWrite; i++) {
-            out.writeByte(0);
+    @Throws(IOException::class)
+    override fun write(output: ByteCodeOutput) {
+        super.write(output)
+
+        val bytesToWrite = paddingBytes(output.bytesWritten)
+        for (i in 0..bytesToWrite - 1) {
+            output.writeByte(0)
         }
     }
-    
-    private int paddingBytes(int bytesCount) {
-        
-        int bytesToPad = 4 - bytesCount % 4;
-        return (bytesToPad == 4) ? 0 : bytesToPad;
+
+    private fun paddingBytes(bytesCount: Int): Int {
+        val bytesToPad = 4 - bytesCount % 4
+        return if (bytesToPad == 4) 0 else bytesToPad
     }
 }
