adfdc66d3e4e00828527fe56679cb15e9998c4a9
ingo <ingo.kegel@ej-technologies.com>
2015-12-13 17:33:42 +0100

    AbstractInstruction j2k (rename)

--- modules/data/src/main/java/org/gjt/jclasslib/bytecode/AbstractInstruction.kt
+++ modules/data/src/main/java/org/gjt/jclasslib/bytecode/AbstractInstruction.kt
@@ -7,11 +7,6 @@
 
 package org.gjt.jclasslib.bytecode;
 
-import org.gjt.jclasslib.io.ByteCodeInput;
-import org.gjt.jclasslib.io.ByteCodeOutput;
-
-import java.io.IOException;
-
 /**
     Base class for all opcode instruction wrappers.
  
