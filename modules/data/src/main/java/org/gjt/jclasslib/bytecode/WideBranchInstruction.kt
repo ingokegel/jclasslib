8e3bd968edf3ca5f7c7bd07b454604973363aa87
ingo <ingo.kegel@ej-technologies.com>
2015-12-19 22:05:30 +0100

    WideBranchInstruction j2k (conversion)

--- modules/data/src/main/java/org/gjt/jclasslib/bytecode/WideBranchInstruction.kt
+++ modules/data/src/main/java/org/gjt/jclasslib/bytecode/WideBranchInstruction.kt
@@ -5,51 +5,35 @@
     version 2 of the license, or (at your option) any later version.
 */
 
-package org.gjt.jclasslib.bytecode;
+package org.gjt.jclasslib.bytecode
 
-import org.gjt.jclasslib.io.ByteCodeInput;
-import org.gjt.jclasslib.io.ByteCodeOutput;
+import org.gjt.jclasslib.io.ByteCodeInput
+import org.gjt.jclasslib.io.ByteCodeOutput
 
-import java.io.IOException;
+import java.io.IOException
 
 /**
-    Describes an instruction that is followed by an immediate int.
- 
-    @author <a href="mailto:jclasslib@ej-technologies.com">Ingo Kegel</a>
-*/
-public class WideBranchInstruction extends AbstractBranchInstruction {
+ * Describes an instruction that is followed by an immediate int.
 
-    /**
-        Constructor.
-        @param opcode the opcode.
-     */
-    public WideBranchInstruction(Opcode opcode) {
-        super(opcode); 
+ * @author [Ingo Kegel](mailto:jclasslib@ej-technologies.com)
+ */
+class WideBranchInstruction @JvmOverloads constructor(opcode: Opcode, branchOffset: Int = 0) : AbstractBranchInstruction(opcode, branchOffset) {
+
+    override val size: Int
+        get() = super.size + 4
+
+    @Throws(IOException::class)
+    override fun read(input: ByteCodeInput) {
+        super.read(input)
+
+        branchOffset = input.readInt()
     }
 
-    /**
-        Constructor.
-        @param opcode the opcode.
-        @param branchOffset the immediate int value.
-     */
-    public WideBranchInstruction(Opcode opcode, int branchOffset) {
-        super(opcode, branchOffset);
-    }
-    
-    public int getSize() {
-        return super.getSize() + 4;
+    @Throws(IOException::class)
+    override fun write(output: ByteCodeOutput) {
+        super.write(output)
+
+        output.writeInt(branchOffset)
     }
 
-    public void read(ByteCodeInput in) throws IOException {
-        super.read(in);
-
-        setBranchOffset(in.readInt());
-    }
-
-    public void write(ByteCodeOutput out) throws IOException {
-        super.write(out);
-
-        out.writeInt(getBranchOffset());
-    }
-    
 }
