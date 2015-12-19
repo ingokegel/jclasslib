9b24b8c7ea994f1fab9ad71efc2e8f02af15ad7c
ingo <ingo.kegel@ej-technologies.com>
2015-12-19 15:14:40 +0100

    TableSwitchInstruction j2k (conversion)

--- modules/data/src/main/java/org/gjt/jclasslib/bytecode/TableSwitchInstruction.kt
+++ modules/data/src/main/java/org/gjt/jclasslib/bytecode/TableSwitchInstruction.kt
@@ -5,126 +5,70 @@
     version 2 of the license, or (at your option) any later version.
 */
 
-package org.gjt.jclasslib.bytecode;
+package org.gjt.jclasslib.bytecode
 
-import org.gjt.jclasslib.io.ByteCodeInput;
-import org.gjt.jclasslib.io.ByteCodeOutput;
+import org.gjt.jclasslib.io.ByteCodeInput
+import org.gjt.jclasslib.io.ByteCodeOutput
 
-import java.io.IOException;
+import java.io.IOException
 
 /**
-    Describes the <tt>tableswitch</tt> instruction.
- 
-    @author <a href="mailto:jclasslib@ej-technologies.com">Ingo Kegel</a>
-*/
-public class TableSwitchInstruction extends PaddedInstruction {
+ * Describes the tableswitch instruction.
 
-    private int defaultOffset;
-    private int lowByte;
-    private int highByte;
-    private int[] jumpOffsets;
-   
-    /**
-        Constructor.
-        @param opcode the opcode.
-     */
-    public TableSwitchInstruction(Opcode opcode) {
-        super(opcode); 
-    }
-    
-    public int getSize() {
-        return super.getSize() + 12 + 4 * jumpOffsets.length;
-    }
+ * @author [Ingo Kegel](mailto:jclasslib@ej-technologies.com)
+ */
+class TableSwitchInstruction(opcode: Opcode) : PaddedInstruction(opcode) {
 
     /**
-        Get the default offset of the branch of this instruction.
-        @return the offset
+     * Default offset of the branch of this instruction.
      */
-    public int getDefaultOffset() {
-        return defaultOffset;
-    }
+    var defaultOffset: Int = 0
 
     /**
-        Set the default offset of the branch of this instruction.
-        @param defaultOffset the offset
+     * Lower bound for the table switch.
      */
-    public void setDefaultOffset(int defaultOffset) {
-        this.defaultOffset = defaultOffset;
-    }
-    
-    /**
-        Get the lower bound for the table switch.
-        @return the lower bound
-     */
-    public int getLowByte() {
-        return lowByte;
-    }
+    var lowByte: Int = 0
 
     /**
-        Set the lower bound for the table switch.
-        @param lowByte the lower bound
+     * Upper bound for the table switch.
      */
-    public void setLowByte(int lowByte) {
-        this.lowByte = lowByte;
-    }
-    
-    /**
-        Get the upper bound for the table switch.
-        @return the upper bound
-     */
-    public int getHighByte() {
-        return highByte;
-    }
+    var highByte: Int = 0
 
     /**
-        Set the upper bound for the table switch.
-        @param highByte the upper bound
+     * Array of relative jump offsets for the table switch.
      */
-    public void setHighByte(int highByte) {
-        this.highByte = highByte;
-    }
-    
-    /**
-        Get the array of relative jump offsets for the table switch.
-        @return the array
-     */
-    public int[] getJumpOffsets() {
-        return jumpOffsets;
-    }
+    var jumpOffsets: IntArray = IntArray(0)
 
-    /**
-        Set the array of relative jump offsets for the table switch.
-        @param jumpOffsets the array
-     */
-    public void setJumpOffsets(int[] jumpOffsets) {
-        this.jumpOffsets = jumpOffsets;
-    }
-    
-    public void read(ByteCodeInput in) throws IOException {
-        super.read(in);
+    override val size: Int
+        get() = super.size + 12 + 4 * jumpOffsets.size
 
-        defaultOffset = in.readInt();
-        lowByte = in.readInt();
-        highByte = in.readInt();
+    @Throws(IOException::class)
+    override fun read(input: ByteCodeInput) {
+        super.read(input)
 
-        int numberOfOffsets = highByte - lowByte + 1;
-        jumpOffsets = new int[numberOfOffsets];
-        
-        for (int i = 0; i < numberOfOffsets; i++) {
-            jumpOffsets[i] = in.readInt();
+        defaultOffset = input.readInt()
+        lowByte = input.readInt()
+        highByte = input.readInt()
+
+        val numberOfOffsets = highByte - lowByte + 1
+        jumpOffsets = IntArray(numberOfOffsets)
+
+        for (i in 0..numberOfOffsets - 1) {
+            jumpOffsets[i] = input.readInt()
         }
-        
+
     }
 
-    public void write(ByteCodeOutput out) throws IOException {
-        super.write(out);
+    @Throws(IOException::class)
+    override fun write(output: ByteCodeOutput) {
+        super.write(output)
 
-        out.writeInt(defaultOffset);
-        out.writeInt(lowByte);
-        out.writeInt(highByte);
+        output.writeInt(defaultOffset)
+        output.writeInt(lowByte)
+        output.writeInt(highByte)
 
-        for (int jumpOffset : jumpOffsets) {
-            out.writeInt(jumpOffset);
+        for (jumpOffset in jumpOffsets) {
+            output.writeInt(jumpOffset)
         }
     }
 
