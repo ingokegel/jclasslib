cf13d2e575e5a5d9c7ca31f260c2e8cd76a8552d
ingo <ingo.kegel@ej-technologies.com>
2015-12-20 21:54:01 +0100

    ConstantDoubleInfo j2k (conversion)

--- modules/data/src/main/java/org/gjt/jclasslib/structures/constants/ConstantDoubleInfo.kt
+++ modules/data/src/main/java/org/gjt/jclasslib/structures/constants/ConstantDoubleInfo.kt
@@ -5,66 +5,59 @@
     version 2 of the license, or (at your option) any later version.
 */
 
-package org.gjt.jclasslib.structures.constants;
+package org.gjt.jclasslib.structures.constants
 
-import org.gjt.jclasslib.structures.ConstantType;
-import org.gjt.jclasslib.structures.InvalidByteCodeException;
+import org.gjt.jclasslib.structures.ConstantType
+import org.gjt.jclasslib.structures.InvalidByteCodeException
 
-import java.io.DataInput;
-import java.io.DataOutput;
-import java.io.IOException;
+import java.io.DataInput
+import java.io.DataOutput
+import java.io.IOException
+import java.lang.Double.doubleToLongBits
+import java.lang.Double.longBitsToDouble
 
 /**
-    Describes a <tt>CONSTANT_Double_info</tt> constant pool data structure.
- 
-    @author <a href="mailto:jclasslib@ej-technologies.com">Ingo Kegel</a>
-*/
-public class ConstantDoubleInfo extends ConstantLargeNumeric {
+ * Describes a CONSTANT_Double_info constant pool data structure.
 
-    public ConstantType getConstantType() {
-        return ConstantType.CONSTANT_DOUBLE;
-    }
+ * @author [Ingo Kegel](mailto:jclasslib@ej-technologies.com)
+ */
+class ConstantDoubleInfo : ConstantLargeNumeric() {
 
-    public String getVerbose() throws InvalidByteCodeException {
-        return String.valueOf(getDouble());
-    }
-    
-    /**
-        Get the double value of this constant pool entry.
-        @return the value
-     */
-    public double getDouble() {
-        long longBits = (long)getHighBytes() << 32 | (long)getLowBytes() & 0xFFFFFFFFL;
-        return Double.longBitsToDouble(longBits);
-    }
+    override val constantType: ConstantType
+        get() = ConstantType.CONSTANT_DOUBLE
+
+    override val verbose: String
+        @Throws(InvalidByteCodeException::class)
+        get() = double.toString()
 
     /**
-        Set the double value of this constant pool entry.
-        @param number the value
+     * The double value of this constant pool entry.
      */
-    public void setDouble(double number) {
-        long longBits = Double.doubleToLongBits(number);
-        setHighBytes((int)(longBits >>> 32 & 0xFFFFFFFFL));
-        setLowBytes((int)(longBits & 0xFFFFFFFFL));
+    var double: Double
+        get() {
+            val longBits = highBytes.toLong() shl 32 or (lowBytes.toLong() and 0xFFFFFFFFL)
+            return longBitsToDouble(longBits)
+        }
+        set(number) {
+            val longBits = doubleToLongBits(number)
+            highBytes = (longBits ushr 32 and 0xFFFFFFFFL).toInt()
+            lowBytes = (longBits and 0xFFFFFFFFL).toInt()
+        }
+
+    @Throws(InvalidByteCodeException::class, IOException::class)
+    override fun read(input: DataInput) {
+        super.read(input)
+        if (isDebug) debug("read")
     }
 
-    public void read(DataInput in)
-        throws InvalidByteCodeException, IOException {
-        
-        super.read(in);
-        if (isDebug()) debug("read ");
+    @Throws(InvalidByteCodeException::class, IOException::class)
+    override fun write(output: DataOutput) {
+        output.writeByte(ConstantType.CONSTANT_DOUBLE.tag)
+        super.write(output)
+        if (isDebug) debug("wrote")
     }
 
-    public void write(DataOutput out)
-        throws InvalidByteCodeException, IOException {
-        
-        out.writeByte(ConstantType.CONSTANT_DOUBLE.getTag());
-        super.write(out);
-        if (isDebug()) debug("wrote ");
-    }
-    
-    protected void debug(String message) {
-        super.debug(message + getConstantType() + " with high_bytes " + getHighBytes() +
-              " and low_bytes " + getLowBytes());
+    override fun debug(message: String) {
+        super.debug("$message $constantType with high_bytes $highBytes and low_bytes $lowBytes")
     }
 }
