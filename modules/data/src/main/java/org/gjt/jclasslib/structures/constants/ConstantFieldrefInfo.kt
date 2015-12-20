fc9182f80335bd9193c9fd06786f3a569255d5be
ingo <ingo.kegel@ej-technologies.com>
2015-12-20 21:48:26 +0100

    ConstantFieldrefInfo j2k (conversion)

--- modules/data/src/main/java/org/gjt/jclasslib/structures/constants/ConstantFieldrefInfo.kt
+++ modules/data/src/main/java/org/gjt/jclasslib/structures/constants/ConstantFieldrefInfo.kt
@@ -5,45 +5,40 @@
     version 2 of the license, or (at your option) any later version.
 */
 
-package org.gjt.jclasslib.structures.constants;
+package org.gjt.jclasslib.structures.constants
 
-import org.gjt.jclasslib.structures.ConstantType;
-import org.gjt.jclasslib.structures.InvalidByteCodeException;
-import org.jetbrains.annotations.NotNull;
+import org.gjt.jclasslib.structures.ConstantType
+import org.gjt.jclasslib.structures.InvalidByteCodeException
 
-import java.io.DataInput;
-import java.io.DataOutput;
-import java.io.IOException;
+import java.io.DataInput
+import java.io.DataOutput
+import java.io.IOException
 
 /**
-    Describes a <tt>CONSTANT_Fieldref_info</tt> constant pool data structure.
+ * Describes a CONSTANT_Fieldref_info constant pool data structure.
 
-    @author <a href="mailto:jclasslib@ej-technologies.com">Ingo Kegel</a>
-*/
-public class ConstantFieldrefInfo extends ConstantReference {
-    
-    public ConstantType getConstantType() {
-        return ConstantType.CONSTANT_FIELDREF;
+ * @author [Ingo Kegel](mailto:jclasslib@ej-technologies.com)
+ */
+class ConstantFieldrefInfo : ConstantReference() {
+
+    override val constantType: ConstantType
+        get() = ConstantType.CONSTANT_FIELDREF
+
+    @Throws(InvalidByteCodeException::class, IOException::class)
+    override fun read(input: DataInput) {
+        super.read(input)
+        if (isDebug) debug("read")
     }
 
-    public void read(DataInput in)
-        throws InvalidByteCodeException, IOException {
-        
-        super.read(in);
-        if (isDebug()) debug("read ");
+    @Throws(InvalidByteCodeException::class, IOException::class)
+    override fun write(output: DataOutput) {
+        output.writeByte(ConstantType.CONSTANT_FIELDREF.tag)
+        super.write(output)
+        if (isDebug) debug("wrote")
     }
 
-    public void write(DataOutput out)
-        throws InvalidByteCodeException, IOException {
-        
-        out.writeByte(ConstantType.CONSTANT_FIELDREF.getTag());
-        super.write(out);
-        if (isDebug()) debug("wrote ");
-    }
-    
-    protected void debug(String message) {
-        super.debug(message + getConstantType() + " with class_index " + getClassIndex() +
-              " and name_and_type_index " + getNameAndTypeIndex());
+    override fun debug(message: String) {
+        super.debug("$message $constantType with class_index $classIndex and name_and_type_index $nameAndTypeIndex")
     }
 
 }
