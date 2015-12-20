bd653d36c598ee4bb7f150eb8057e16dd66719af
ingo <ingo.kegel@ej-technologies.com>
2015-12-20 21:57:11 +0100

    ConstantLongInfo j2k (conversion)

--- modules/data/src/main/java/org/gjt/jclasslib/structures/constants/ConstantLongInfo.kt
+++ modules/data/src/main/java/org/gjt/jclasslib/structures/constants/ConstantLongInfo.kt
@@ -5,65 +5,55 @@
     version 2 of the license, or (at your option) any later version.
 */
 
-package org.gjt.jclasslib.structures.constants;
+package org.gjt.jclasslib.structures.constants
 
-import org.gjt.jclasslib.structures.ConstantType;
-import org.gjt.jclasslib.structures.InvalidByteCodeException;
+import org.gjt.jclasslib.structures.ConstantType
+import org.gjt.jclasslib.structures.InvalidByteCodeException
 
-import java.io.DataInput;
-import java.io.DataOutput;
-import java.io.IOException;
+import java.io.DataInput
+import java.io.DataOutput
+import java.io.IOException
 
 /**
-    Describes a <tt>CONSTANT_Long_info</tt> constant pool data structure.
- 
-    @author <a href="mailto:jclasslib@ej-technologies.com">Ingo Kegel</a>
-*/
-public class ConstantLongInfo extends ConstantLargeNumeric {
+ * Describes a CONSTANT_Long_info constant pool data structure.
 
-    public ConstantType getConstantType() {
-        return ConstantType.CONSTANT_LONG;
-    }
+ * @author [Ingo Kegel](mailto:jclasslib@ej-technologies.com)
+ */
+class ConstantLongInfo : ConstantLargeNumeric() {
 
-    public String getVerbose() throws InvalidByteCodeException {
-        return String.valueOf(getLong());
-    }
+    override val constantType: ConstantType
+        get() = ConstantType.CONSTANT_LONG
 
-    /**
-        Get the long value of this constant pool entry.
-        @return the value
-     */
-    public long getLong() {
-        return ((long)getHighBytes() << 32) | ((long)getLowBytes() & 0xFFFFFFFF);
-    }
+    override val verbose: String
+        @Throws(InvalidByteCodeException::class)
+        get() = long.toString()
 
     /**
-        Set the long value of this constant pool entry.
-        @param number the value
+     * The long value of this constant pool entry.
+     * @param number the value
      */
-    public void setLong(long number) {
-        setHighBytes((int)(number >>> 32));
-        setLowBytes((int)(number & 0xFFFFFFFF));
+    var long: Long
+        get() = (highBytes.toLong() shl 32) or (lowBytes.toLong() and 0xFFFFFFFF)
+        set(number) {
+            highBytes = (number ushr 32).toInt()
+            lowBytes = (number and 0xFFFFFFFF).toInt()
+        }
+
+    @Throws(InvalidByteCodeException::class, IOException::class)
+    override fun read(input: DataInput) {
+        super.read(input)
+        if (isDebug) debug("read")
     }
 
-    public void read(DataInput in)
-        throws InvalidByteCodeException, IOException {
-        
-        super.read(in);
-        if (isDebug()) debug("read ");
+    @Throws(InvalidByteCodeException::class, IOException::class)
+    override fun write(output: DataOutput) {
+        output.writeByte(ConstantType.CONSTANT_LONG.tag)
+        super.write(output)
+        if (isDebug) debug("wrote")
     }
-    
-    public void write(DataOutput out)
-        throws InvalidByteCodeException, IOException {
-        
-        out.writeByte(ConstantType.CONSTANT_LONG.getTag());
-        super.write(out);
-        if (isDebug()) debug("wrote ");
-    }
-    
-    protected void debug(String message) {
-        super.debug(message + getConstantType() + " with high_bytes " + getHighBytes() +
-              " and low_bytes " + getLowBytes());
+
+    override fun debug(message: String) {
+        super.debug("$message $constantType with high_bytes $highBytes and low_bytes $lowBytes")
     }
 
 }
