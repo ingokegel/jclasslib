582c0d486f3e81fe03d2cafc2e1eb94c9efd116b
ingo <ingo.kegel@ej-technologies.com>
2015-12-25 11:17:40 +0100

    ExceptionsAttribute j2k (conversion)

--- modules/data/src/main/java/org/gjt/jclasslib/structures/attributes/ExceptionsAttribute.kt
+++ modules/data/src/main/java/org/gjt/jclasslib/structures/attributes/ExceptionsAttribute.kt
@@ -5,75 +5,55 @@
     version 2 of the license, or (at your option) any later version.
 */
 
-package org.gjt.jclasslib.structures.attributes;
+package org.gjt.jclasslib.structures.attributes
 
-import org.gjt.jclasslib.structures.AttributeInfo;
-import org.gjt.jclasslib.structures.InvalidByteCodeException;
+import org.gjt.jclasslib.structures.AttributeInfo
+import org.gjt.jclasslib.structures.InvalidByteCodeException
 
-import java.io.DataInput;
-import java.io.DataOutput;
-import java.io.IOException;
+import java.io.DataInput
+import java.io.DataOutput
+import java.io.IOException
 
 /**
-    Describes an <tt>Exceptions</tt> attribute structure.
+ * Describes an Exceptions attribute structure.
 
-    @author <a href="mailto:jclasslib@ej-technologies.com">Ingo Kegel</a>
-*/
-public class ExceptionsAttribute extends AttributeInfo {
-
-    /** Name of the attribute as in the corresponding constant pool entry. */
-    public static final String ATTRIBUTE_NAME = "Exceptions";
-
-    private static final int INITIAL_LENGTH = 2;
-    
-    private int[] exceptionIndexTable;
-    
-    /**
-        Get the list of exceptions thrown by the parent <tt>Code</tt> attribute
-        as an array of indices into the constant pool.
-        @return the array
-     */
-    public int[] getExceptionIndexTable() {
-        return exceptionIndexTable;
-    }
+ * @author [Ingo Kegel](mailto:jclasslib@ej-technologies.com)
+ */
+class ExceptionsAttribute : AttributeInfo() {
 
     /**
-        Set the list of exceptions thrown by the parent <tt>Code</tt> attribute
-        as an array of indices into the constant pool.
-        @param exceptionIndexTable the array
+     * Exceptions thrown by the parent code attribute
      */
-    public void setExceptionIndexTable(int[] exceptionIndexTable) {
-        this.exceptionIndexTable = exceptionIndexTable;
-    }
+    var exceptionIndexTable: IntArray = IntArray(0)
 
-    public void read(DataInput in) throws InvalidByteCodeException, IOException {
-            
-        int numberOfExceptions = in.readUnsignedShort();
-        exceptionIndexTable = new int[numberOfExceptions];
-        for (int i = 0 ; i < numberOfExceptions; i++) {
-            exceptionIndexTable[i] = in.readUnsignedShort();
+    @Throws(InvalidByteCodeException::class, IOException::class)
+    override fun read(input: DataInput) {
+        val numberOfExceptions = input.readUnsignedShort()
+        exceptionIndexTable = IntArray(numberOfExceptions) {
+            input.readUnsignedShort()
         }
-        
-        if (isDebug()) debug("read ");
+
+        if (isDebug) debug("read")
     }
 
-    public void write(DataOutput out) throws InvalidByteCodeException, IOException {
+    @Throws(InvalidByteCodeException::class, IOException::class)
+    override fun write(output: DataOutput) {
+        output.writeShort(exceptionIndexTable.size)
+        exceptionIndexTable.forEach { output.writeShort(it) }
 
-        int numberOfExceptions = getLength(exceptionIndexTable);
-
-        out.writeShort(numberOfExceptions);
-        for (int i = 0 ; i < numberOfExceptions; i++) {
-            out.writeShort(exceptionIndexTable[i]);
-        }
-        if (isDebug()) debug("wrote ");
+        if (isDebug) debug("wrote")
     }
 
-    public int getAttributeLength() {
-        return INITIAL_LENGTH + 2 * getLength(exceptionIndexTable);
+    override fun getAttributeLength(): Int = 2 + 2 * exceptionIndexTable.size
+
+    override fun debug(message: String) {
+        super.debug("$message Exception attribute with ${getLength(exceptionIndexTable)} exceptions")
     }
 
-    protected void debug(String message) {
-        super.debug(message + "Exception attribute with " + getLength(exceptionIndexTable) + " exceptions");
+    companion object {
+        /**
+         * Name of the attribute as in the corresponding constant pool entry.
+         */
+        val ATTRIBUTE_NAME = "Exceptions"
     }
-
 }
