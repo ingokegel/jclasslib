ca3bbdf3ddeb8d3065b774665a1c54759151d5dd
ingo <ingo.kegel@ej-technologies.com>
2015-12-27 15:06:38 +0100

    TypeParameterBoundTargetInfo j2k (conversion)

--- modules/data/src/main/java/org/gjt/jclasslib/structures/attributes/targettype/TypeParameterBoundTargetInfo.kt
+++ modules/data/src/main/java/org/gjt/jclasslib/structures/attributes/targettype/TypeParameterBoundTargetInfo.kt
@@ -5,57 +5,37 @@
  version 2 of the license, or (at your option) any later version.
  */
 
-package org.gjt.jclasslib.structures.attributes.targettype;
+package org.gjt.jclasslib.structures.attributes.targettype
 
-import org.gjt.jclasslib.structures.InvalidByteCodeException;
+import org.gjt.jclasslib.structures.InvalidByteCodeException
 
-import java.io.DataInput;
-import java.io.DataOutput;
-import java.io.IOException;
+import java.io.DataInput
+import java.io.DataOutput
+import java.io.IOException
 
 /**
- * Target info for a <tt>TypeAnnotation</tt> structure with a parameter index an a bound index.
+ * Target info for a TypeAnnotation structure with a parameter index an a bound index.
  */
-public class TypeParameterBoundTargetInfo extends TargetInfo {
+class TypeParameterBoundTargetInfo : TargetInfo() {
 
-    private int typeParameterIndex;
-    private int boundIndex;
+    var typeParameterIndex: Int = 0
+    var boundIndex: Int = 0
 
-    public int getTypeParameterIndex() {
-        return typeParameterIndex;
+    @Throws(InvalidByteCodeException::class, IOException::class)
+    override fun read(input: DataInput) {
+        typeParameterIndex = input.readUnsignedByte()
+        boundIndex = input.readUnsignedByte()
     }
 
-    public void setTypeParameterIndex(int typeParameterIndex) {
-        this.typeParameterIndex = typeParameterIndex;
+    @Throws(InvalidByteCodeException::class, IOException::class)
+    override fun write(output: DataOutput) {
+        output.writeByte(typeParameterIndex)
+        output.writeByte(boundIndex)
     }
 
-    public int getBoundIndex() {
-        return boundIndex;
-    }
+    override val length: Int
+        get() = 2
 
-    public void setBoundIndex(int boundIndex) {
-        this.boundIndex = boundIndex;
-    }
-
-    @Override
-    public void read(DataInput in) throws InvalidByteCodeException, IOException {
-        typeParameterIndex = in.readUnsignedByte();
-        boundIndex = in.readUnsignedByte();
-    }
-
-    @Override
-    public void write(DataOutput out) throws InvalidByteCodeException, IOException {
-        out.writeByte(typeParameterIndex);
-        out.writeByte(boundIndex);
-    }
-
-    @Override
-    public int getLength() {
-        return 2;
-    }
-
-    @Override
-    public String getVerbose() {
-        return "parameter index " + typeParameterIndex + ", bound index " + boundIndex;
-    }
+    override val verbose: String
+        get() = "parameter index $typeParameterIndex, bound index $boundIndex"
 }
