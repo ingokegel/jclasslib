d2494de5bcc244cec39fc4a7b72fa51f45d8af5f
ingo <ingo.kegel@ej-technologies.com>
2015-12-27 21:58:32 +0100

    TypePathKind j2k (conversion)

--- modules/data/src/main/java/org/gjt/jclasslib/structures/attributes/TypePathKind.kt
+++ modules/data/src/main/java/org/gjt/jclasslib/structures/attributes/TypePathKind.kt
@@ -5,40 +5,22 @@
  version 2 of the license, or (at your option) any later version.
  */
 
-package org.gjt.jclasslib.structures.attributes;
+package org.gjt.jclasslib.structures.attributes
 
-import org.gjt.jclasslib.structures.InvalidByteCodeException;
+import org.gjt.jclasslib.structures.ClassFileEnum
+import org.gjt.jclasslib.structures.Lookup
 
 /**
- * Describes the kind of a type path in a <tt>TypePathEntry</tt> structure.
+ * Describes the kind of a type path in a TypePathEntry structure.
  */
-public enum TypePathKind {
+enum class TypePathKind(override val tag: Int) : ClassFileEnum {
     DEEPER_IN_ARRAY_TYPE(0),
     DEEPER_IN_NESTED_TYPE(1),
     WILDCARD_BOUND(2),
     TYPE_ARGUMENT(3);
 
-    private static TypePathKind[] typePathKinds = new TypePathKind[values().length];
-    static {
-        for (int i = 0; i < values().length; i++) {
-            typePathKinds[i] = values()[i];
-        }
-    }
 
-    public static TypePathKind getFromTag(int tag) throws InvalidByteCodeException {
-        if (tag < 0 || tag >= typePathKinds.length) {
-            throw new InvalidByteCodeException("Invalid type path kind: " + tag);
-        }
-        return typePathKinds[tag];
-    }
+    companion object : Lookup<TypePathKind>(TypePathKind::class.java, "type path kind") {
 
-    private int tag;
-
-    TypePathKind(int tag) {
-        this.tag = tag;
-    }
-
-    public int getTag() {
-        return tag;
     }
 }
