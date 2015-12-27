faea9fd430fdeb33a9f246befba7e9023b4d21e6
ingo <ingo.kegel@ej-technologies.com>
2015-12-27 15:02:19 +0100

    TypeArgumentTargetInfo j2k (conversion)

--- modules/data/src/main/java/org/gjt/jclasslib/structures/attributes/targettype/TypeArgumentTargetInfo.kt
+++ modules/data/src/main/java/org/gjt/jclasslib/structures/attributes/targettype/TypeArgumentTargetInfo.kt
@@ -5,56 +5,36 @@
  version 2 of the license, or (at your option) any later version.
  */
 
-package org.gjt.jclasslib.structures.attributes.targettype;
+package org.gjt.jclasslib.structures.attributes.targettype
 
-import org.gjt.jclasslib.structures.InvalidByteCodeException;
+import org.gjt.jclasslib.structures.InvalidByteCodeException
 
-import java.io.DataInput;
-import java.io.DataOutput;
-import java.io.IOException;
+import java.io.DataInput
+import java.io.DataOutput
+import java.io.IOException
 
 /**
- * Target info for a <tt>TypeAnnotation</tt> structure with an offset and an argument index.
+ * Target info for a TypeAnnotation structure with an offset and an argument index.
  */
-public class TypeArgumentTargetInfo extends TargetInfo {
-    private int offset;
-    private int typeArgumentIndex;
+class TypeArgumentTargetInfo : TargetInfo() {
+    var offset: Int = 0
+    var typeArgumentIndex: Int = 0
 
-    public int getOffset() {
-        return offset;
+    @Throws(InvalidByteCodeException::class, IOException::class)
+    override fun read(input: DataInput) {
+        offset = input.readUnsignedShort()
+        typeArgumentIndex = input.readByte().toInt()
     }
 
-    public void setOffset(int offset) {
-        this.offset = offset;
+    @Throws(InvalidByteCodeException::class, IOException::class)
+    override fun write(output: DataOutput) {
+        output.writeShort(offset)
+        output.writeByte(typeArgumentIndex)
     }
 
-    public int getTypeArgumentIndex() {
-        return typeArgumentIndex;
-    }
+    override val length: Int
+        get() = 3
 
-    public void setTypeArgumentIndex(int typeArgumentIndex) {
-        this.typeArgumentIndex = typeArgumentIndex;
-    }
-
-    @Override
-    public void read(DataInput in) throws InvalidByteCodeException, IOException {
-        offset = in.readUnsignedShort();
-        typeArgumentIndex = in.readByte();
-    }
-
-    @Override
-    public void write(DataOutput out) throws InvalidByteCodeException, IOException {
-        out.writeShort(offset);
-        out.writeByte(typeArgumentIndex);
-    }
-
-    @Override
-    public int getLength() {
-        return 3;
-    }
-
-    @Override
-    public String getVerbose() {
-        return "offset " + offset + ", type argument index " + typeArgumentIndex;
-    }
+    override val verbose: String
+        get() = "offset $offset, type argument index $typeArgumentIndex"
 }
