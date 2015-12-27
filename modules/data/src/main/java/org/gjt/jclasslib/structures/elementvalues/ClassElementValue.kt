9c2e76057fb7df4b1c66e71383b3aab74cf1cf43
ingo <ingo.kegel@ej-technologies.com>
2015-12-27 22:47:52 +0100

    ClassElementValue j2k (conversion)

--- modules/data/src/main/java/org/gjt/jclasslib/structures/elementvalues/ClassElementValue.kt
+++ modules/data/src/main/java/org/gjt/jclasslib/structures/elementvalues/ClassElementValue.kt
@@ -4,76 +4,48 @@
     License as published by the Free Software Foundation; either
     version 2 of the license, or (at your option) any later version.
 */
-package org.gjt.jclasslib.structures.elementvalues;
+package org.gjt.jclasslib.structures.elementvalues
 
-import org.gjt.jclasslib.structures.InvalidByteCodeException;
+import org.gjt.jclasslib.structures.InvalidByteCodeException
 
-import java.io.DataInput;
-import java.io.DataOutput;
-import java.io.IOException;
+import java.io.DataInput
+import java.io.DataOutput
+import java.io.IOException
 
 /**
- * Describes an  <tt>ClassElementValue</tt> attribute structure.
- *
- * @author <a href="mailto:vitor.carreira@gmail.com">Vitor Carreira</a>
- *
+ * Describes an  ClassElementValue attribute structure.
+
+ * @author [Vitor Carreira](mailto:vitor.carreira@gmail.com)
  */
-public class ClassElementValue extends ElementValue {
-
-    public final static String ENTRY_NAME = "ClassElement";
-
-    private static final int LENGTH = 2;
-    private int classInfoIndex;
-
-    protected ClassElementValue() {
-        super(ElementValueType.CLASS);
-    }
-
+class ClassElementValue : ElementValue(ElementValueType.CLASS) {
     /**
-     * Get the <tt>class_info_index</tt> of this element value entry.
-     *
-     * @return the <tt>class_info_index</tt>
+     * class_info_index of this element value entry.
      */
-    public int getClassInfoIndex() {
-        return this.classInfoIndex;
+    var classInfoIndex: Int = 0
+
+    override val specificLength: Int
+        get() = 2
+
+    @Throws(InvalidByteCodeException::class, IOException::class)
+    override fun read(input: DataInput) {
+        classInfoIndex = input.readUnsignedShort()
+
+        if (isDebug) debug("read")
     }
 
-    /**
-     * Set the <tt>class_info_index</tt> of this element value entry.
-     *
-     * @param classInfoIndex the <tt>class_info_index</tt>
-     */
-    public void setClassInfoIndex(int classInfoIndex) {
-        this.classInfoIndex = classInfoIndex;
+    @Throws(InvalidByteCodeException::class, IOException::class)
+    override fun write(output: DataOutput) {
+        super.write(output)
+        output.writeShort(classInfoIndex)
+
+        if (isDebug) debug("wrote")
     }
 
-    protected int getSpecificLength() {
-        return LENGTH;
+    override fun debug(message: String) {
+        super.debug("$message ClassElementValue with class_info_index $classInfoIndex")
     }
 
-    public void read(DataInput in) throws InvalidByteCodeException, IOException {
-
-        classInfoIndex = in.readUnsignedShort();
-
-        if (isDebug()) debug("read ");
-    }
-
-    public void write(DataOutput out) throws InvalidByteCodeException, IOException {
-        super.write(out);
-
-        out.writeShort(classInfoIndex);
-
-        if (isDebug()) debug("wrote ");
-    }
-
-    protected void debug(String message) {
-        super.debug(message +
-                "ClassElementValue with class_info_index " +
-                classInfoIndex);
-    }
-
-    public String getEntryName() {
-        return ENTRY_NAME;
-    }
+    override val entryName: String
+        get() = "ClassElement"
 
 }
