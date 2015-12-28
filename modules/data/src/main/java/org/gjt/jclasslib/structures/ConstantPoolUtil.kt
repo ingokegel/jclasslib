afa9469627c3fdb296ba9b40172b24307f29c02e
ingo <ingo.kegel@ej-technologies.com>
2015-12-28 22:18:56 +0100

    ConstantPoolUtil j2k (conversion)

--- modules/data/src/main/java/org/gjt/jclasslib/structures/ConstantPoolUtil.kt
+++ modules/data/src/main/java/org/gjt/jclasslib/structures/ConstantPoolUtil.kt
@@ -5,180 +5,149 @@
     version 2 of the license, or (at your option) any later version.
 */
 
-package org.gjt.jclasslib.structures;
+package org.gjt.jclasslib.structures
 
-import org.gjt.jclasslib.structures.constants.*;
+import org.gjt.jclasslib.structures.constants.*
 
 /**
-    Utility methods for working on the constant pool of a <tt>ClassFile</tt>
-    object.
+ * Utility methods for working on the constant pool of a ClassFile
+ * object.
 
-    @author <a href="mailto:jclasslib@ej-technologies.com">Ingo Kegel</a>
-*/
+ * @author [Ingo Kegel](mailto:jclasslib@ej-technologies.com)
+ */
 
-public class ConstantPoolUtil {
+object ConstantPoolUtil {
 
-    private ConstantPoolUtil() {
+    /**
+     * Add a ConstantMethodRef constant pool entry to the constant pool
+     * of a ClassFile.
+     * @param classFile the class file whose constant pool is to be edited
+     * @param className the name of the referenced class
+     * @param methodName the name of the referenced method
+     * @param methodSignature the signature of the referenced method
+     * @param sizeDelta the minimum increment by which the array holding the
+     * constant pool is to be enlarged. Set to 0 if unsure.
+     * @return the constant pool index of the added ConstantMethodRef
+     */
+    fun addConstantMethodrefInfo(classFile: ClassFile, className: String, methodName: String, methodSignature: String): Int {
+        val classIndex = addConstantClassInfo(classFile, className)
+        val nameAndTypeIndex = addConstantNameAndTypeInfo(classFile, methodName, methodSignature)
+
+        val methodrefInfo = ConstantMethodrefInfo().apply {
+            this.classFile = classFile
+            this.classIndex = classIndex
+            this.nameAndTypeIndex = nameAndTypeIndex
+
+        }
+        return addConstantPoolEntry(classFile, methodrefInfo)
     }
 
     /**
-        Add a <tt>ConstantMethodRef</tt> constant pool entry to the constant pool
-        of a <tt>ClassFile</tt>.
-        @param classFile the class file whose constant pool is to be edited
-        @param className the name of the referenced class
-        @param methodName the name of the referenced method
-        @param methodSignature the signature of the referenced method
-        @param sizeDelta the minimum increment by which the array holding the
-                         constant pool is to be enlarged. Set to 0 if unsure.
-        @return the constant pool index of the added <tt>ConstantMethodRef</tt>
+     * Add a ConstantFieldRef constant pool entry to the constant pool
+     * of a ClassFile.
+     * @param classFile the class file whose constant pool is to be edited
+     * @param className the name of the referenced class
+     * @param fieldName the name of the referenced field
+     * @param fieldType the type of the referenced field
+     * @param sizeDelta the minimum increment by which the array holding the
+     * constant pool is to be enlarged. Set to 0 if unsure.
+     * @return the constant pool index of the added ConstantMethodRef
      */
-    public static int addConstantMethodrefInfo(ClassFile classFile,
-                                               String className,
-                                               String methodName,
-                                               String methodSignature,
-                                               int sizeDelta)
-    {
-        sizeDelta = Math.max(sizeDelta, 6);
-        int classIndex = addConstantClassInfo(classFile, className, sizeDelta);
-        int nameAndTypeIndex = addConstantNameAndTypeInfo(classFile, methodName, methodSignature, sizeDelta);
+    fun addConstantFieldrefInfo(classFile: ClassFile, className: String, fieldName: String, fieldType: String): Int {
+        val classIndex = addConstantClassInfo(classFile, className)
+        val nameAndTypeIndex = addConstantNameAndTypeInfo(classFile, fieldName, fieldType)
 
-        ConstantMethodrefInfo methodrefInfo = new ConstantMethodrefInfo();
-        methodrefInfo.setClassFile(classFile);
-        methodrefInfo.setClassIndex(classIndex);
-        methodrefInfo.setNameAndTypeIndex(nameAndTypeIndex);
-        return addConstantPoolEntry(classFile, methodrefInfo, sizeDelta);
+        val fieldrefInfo = ConstantFieldrefInfo().apply {
+            this.classFile = classFile
+            this.classIndex = classIndex
+            this.nameAndTypeIndex = nameAndTypeIndex
+        }
+        return addConstantPoolEntry(classFile, fieldrefInfo)
     }
 
     /**
-        Add a <tt>ConstantFieldRef</tt> constant pool entry to the constant pool
-        of a <tt>ClassFile</tt>.
-        @param classFile the class file whose constant pool is to be edited
-        @param className the name of the referenced class
-        @param fieldName the name of the referenced field
-        @param fieldType the type of the referenced field
-        @param sizeDelta the minimum increment by which the array holding the
-                         constant pool is to be enlarged. Set to 0 if unsure.
-        @return the constant pool index of the added <tt>ConstantMethodRef</tt>
+     * Add a ConstantNameAndTypeInfo constant pool entry to the
+     * constant pool of a ClassFile.
+     * @param classFile the class file whose constant pool is to be edited
+     * @param name the name
+     * @param descriptor the descriptor
+     * @param sizeDelta the minimum increment by which the array holding the
+     * constant pool is to be enlarged. Set to 0 if unsure.
+     * @return the constant pool index of the added ConstantNameAndTypeInfo
      */
-    public static int addConstantFieldrefInfo(ClassFile classFile,
-                                              String className,
-                                              String fieldName,
-                                              String fieldType,
-                                              int sizeDelta)
-    {
-        sizeDelta = Math.max(sizeDelta, 6);
-        int classIndex = addConstantClassInfo(classFile, className, sizeDelta);
-        int nameAndTypeIndex = addConstantNameAndTypeInfo(classFile, fieldName, fieldType, sizeDelta);
+    fun addConstantNameAndTypeInfo(classFile: ClassFile, name: String, descriptor: String): Int {
+        val nameIndex = addConstantUTF8Info(classFile, name)
+        val descriptorIndex = addConstantUTF8Info(classFile, descriptor)
 
-        ConstantFieldrefInfo fieldrefInfo = new ConstantFieldrefInfo();
-        fieldrefInfo.setClassFile(classFile);
-        fieldrefInfo.setClassIndex(classIndex);
-        fieldrefInfo.setNameAndTypeIndex(nameAndTypeIndex);
-        return addConstantPoolEntry(classFile, fieldrefInfo, sizeDelta);
+        val nameAndTypeInfo = ConstantNameAndTypeInfo().apply {
+            this.classFile = classFile
+            this.nameIndex = nameIndex
+            this.descriptorIndex = descriptorIndex
+
+        }
+        return addConstantPoolEntry(classFile, nameAndTypeInfo)
     }
 
     /**
-        Add a <tt>ConstantNameAndTypeInfo</tt> constant pool entry to the
-        constant pool of a <tt>ClassFile</tt>.
-        @param classFile the class file whose constant pool is to be edited
-        @param name the name
-        @param descriptor the descriptor
-        @param sizeDelta the minimum increment by which the array holding the
-                         constant pool is to be enlarged. Set to 0 if unsure.
-        @return the constant pool index of the added <tt>ConstantNameAndTypeInfo</tt>
+     * Add a ConstantClassInfo constant pool entry to the
+     * constant pool of a ClassFile.
+     * @param classFile the class file whose constant pool is to be edited
+     * @param className the name of the referenced class
+     * @param sizeDelta the minimum increment by which the array holding the
+     * constant pool is to be enlarged. Set to 0 if unsure.
+     * @return the constant pool index of the added ConstantClassInfo
      */
-    public static int addConstantNameAndTypeInfo(ClassFile classFile,
-                                                 String name,
-                                                 String descriptor,
-                                                 int sizeDelta)
-    {
-        sizeDelta = Math.max(sizeDelta, 3);
-        int nameIndex = addConstantUTF8Info(classFile, name, sizeDelta);
-        int descriptorIndex = addConstantUTF8Info(classFile, descriptor, sizeDelta);
+    fun addConstantClassInfo(classFile: ClassFile, className: String): Int {
+        val nameIndex = addConstantUTF8Info(classFile, className)
 
-        ConstantNameAndTypeInfo nameAndTypeInfo = new ConstantNameAndTypeInfo();
-        nameAndTypeInfo.setClassFile(classFile);
-        nameAndTypeInfo.setNameIndex(nameIndex);
-        nameAndTypeInfo.setDescriptorIndex(descriptorIndex);
-        return addConstantPoolEntry(classFile, nameAndTypeInfo, sizeDelta);
+        val classInfo = ConstantClassInfo().apply {
+            this.classFile = classFile
+            this.nameIndex = nameIndex
+        }
+        return addConstantPoolEntry(classFile, classInfo)
     }
 
     /**
-        Add a <tt>ConstantClassInfo</tt> constant pool entry to the
-        constant pool of a <tt>ClassFile</tt>.
-        @param classFile the class file whose constant pool is to be edited
-        @param className the name of the referenced class
-        @param sizeDelta the minimum increment by which the array holding the
-                         constant pool is to be enlarged. Set to 0 if unsure.
-        @return the constant pool index of the added <tt>ConstantClassInfo</tt>
+     * Add a ConstantUTF8Info constant pool entry to the
+     * constant pool of a ClassFile.
+     * @param classFile the class file whose constant pool is to be edited
+     * @param string the string
+     * @param sizeDelta the minimum increment by which the array holding the
+     * constant pool is to be enlarged. Set to 0 if unsure.
+     * @return the constant pool index of the added ConstantUTF8Info
      */
-    public static int addConstantClassInfo(ClassFile classFile,
-                                           String className,
-                                           int sizeDelta)
-    {
-        sizeDelta = Math.max(sizeDelta, 2);
-        int nameIndex = addConstantUTF8Info(classFile, className, sizeDelta);
-
-        ConstantClassInfo classInfo = new ConstantClassInfo();
-        classInfo.setClassFile(classFile);
-        classInfo.setNameIndex(nameIndex);
-        return addConstantPoolEntry(classFile, classInfo, sizeDelta);
+    fun addConstantUTF8Info(classFile: ClassFile, string: String): Int {
+        val utf8Info = ConstantUtf8Info().apply {
+            this.classFile = classFile
+            this.string = string
+        }
+        return addConstantPoolEntry(classFile, utf8Info)
     }
 
     /**
-        Add a <tt>ConstantUTF8Info</tt> constant pool entry to the
-        constant pool of a <tt>ClassFile</tt>.
-        @param classFile the class file whose constant pool is to be edited
-        @param string the string
-        @param sizeDelta the minimum increment by which the array holding the
-                         constant pool is to be enlarged. Set to 0 if unsure.
-        @return the constant pool index of the added <tt>ConstantUTF8Info</tt>
+     * Add a constant pool entry to the
+     * constant pool of a ClassFile.
+     * @param classFile the class file whose constant pool is to be edited
+     * @param newEntry the new constant pool entry
+     * @param sizeDelta the minimum increment by which the array holding the
+     * constant pool is to be enlarged. Set to 0 if unsure.
+     * @return the constant pool index of the added constant pool entry
      */
-    public static int addConstantUTF8Info(ClassFile classFile,
-                                          String string,
-                                          int sizeDelta)
-    {
-        ConstantUtf8Info utf8Info = new ConstantUtf8Info();
-        utf8Info.setClassFile(classFile);
-        utf8Info.setString(string);
-        return addConstantPoolEntry(classFile, utf8Info, sizeDelta);
-    }
-
-    /**
-        Add a constant pool entry to the
-        constant pool of a <tt>ClassFile</tt>.
-        @param classFile the class file whose constant pool is to be edited
-        @param newEntry the new constant pool entry
-        @param sizeDelta the minimum increment by which the array holding the
-                         constant pool is to be enlarged. Set to 0 if unsure.
-        @return the constant pool index of the added constant pool entry
-     */
-    public static int addConstantPoolEntry(ClassFile classFile,
-                                           CPInfo newEntry,
-                                           int sizeDelta)
-    {
-        CPInfo[] constantPool = classFile.getConstantPool();
-
-        int index = classFile.getConstantPoolIndex(newEntry);
+    fun addConstantPoolEntry(classFile: ClassFile, newEntry: CPInfo): Int {
+        val constantPool = classFile.constantPool
+        val index = classFile.getConstantPoolIndex(newEntry)
         if (index > -1) {
-            return index;
+            return index
         }
 
-        int lastFreeIndex;
-        lastFreeIndex = constantPool.length - 1;
-        while (lastFreeIndex >= 0 && constantPool[lastFreeIndex] == null) {
-            lastFreeIndex--;
+        val newConstantPool = Array(constantPool.size + 1) {i ->
+            when (i) {
+                constantPool.size -> newEntry
+                else -> constantPool[i]
+            }
         }
-        if (lastFreeIndex == constantPool.length - 1) {
-            CPInfo[] newConstantPool = new CPInfo[constantPool.length + Math.max(1, sizeDelta)];
-            System.arraycopy(constantPool, 0, newConstantPool, 0, constantPool.length);
-            classFile.enlargeConstantPool(newConstantPool);
-            constantPool = newConstantPool;
-        }
-        int newIndex = lastFreeIndex + 1;
-        constantPool[newIndex] = newEntry;
-        classFile.registerConstantPoolEntry(newIndex);
-        return newIndex;
+        classFile.enlargeConstantPool(newConstantPool)
+        return constantPool.size
     }
 
 }
