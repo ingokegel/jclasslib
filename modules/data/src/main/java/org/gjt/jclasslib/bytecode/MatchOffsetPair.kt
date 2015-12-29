3bca71d34178d6d79c27edfe97a789536adebaa1
ingo <ingo.kegel@ej-technologies.com>
2015-12-29 22:21:59 +0100

    MatchOffsetPair j2k (conversion)

--- modules/data/src/main/java/org/gjt/jclasslib/bytecode/MatchOffsetPair.kt
+++ modules/data/src/main/java/org/gjt/jclasslib/bytecode/MatchOffsetPair.kt
@@ -5,58 +5,11 @@
     version 2 of the license, or (at your option) any later version.
 */
 
-package org.gjt.jclasslib.bytecode;
+package org.gjt.jclasslib.bytecode
 
 /**
-    Holds a single match-offset pair.
+ * Holds a single match-offset pair.
 
-    @author <a href="mailto:jclasslib@ej-technologies.com">Ingo Kegel</a>
-*/
-public class MatchOffsetPair {
-
-    private int match;
-    private int offset;
-
-    /**
-     * Constructor.
-     * @param match the match value.
-     * @param offset the branch offset.
-     */
-    public MatchOffsetPair(int match, int offset) {
-        this.match = match;
-        this.offset = offset;
-    }
-
-    /**
-        Get the match value of this match-offset pair.
-        @return the value
-     */
-    public int getMatch() {
-        return match;
-    }
-
-    /**
-        Set the match value of this match-offset pair.
-        @param match the value
-     */
-    public void setMatch(int match) {
-        this.match = match;
-    }
-
-    /**
-        Get the offset of the branch for this match-offset pair.
-        @return the offset
-     */
-    public int getOffset() {
-        return offset;
-    }
-
-    /**
-        Set the offset of the branch for this match-offset pair.
-        @param offset the offset
-     */
-    public void setOffset(int offset) {
-        this.offset = offset;
-    }
-
-}
+ * @author [Ingo Kegel](mailto:jclasslib@ej-technologies.com)
+ */
+class MatchOffsetPair(var match: Int, var offset: Int)
