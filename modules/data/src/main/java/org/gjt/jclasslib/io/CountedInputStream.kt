ffc01d2ae3cdec02a36b5c6b58f9ed9977365afb
ingo <ingo.kegel@ej-technologies.com>
2015-12-29 22:03:20 +0100

    CountedInputStream j2k (conversion)

--- modules/data/src/main/java/org/gjt/jclasslib/io/CountedInputStream.kt
+++ modules/data/src/main/java/org/gjt/jclasslib/io/CountedInputStream.kt
@@ -5,64 +5,54 @@
     version 2 of the license, or (at your option) any later version.
 */
 
-package org.gjt.jclasslib.io;
+package org.gjt.jclasslib.io
 
-import java.io.FilterInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.FilterInputStream
+import java.io.IOException
+import java.io.InputStream
 
 /**
-    <tt>InputStream</tt> which counts the number of bytes read.
- 
-    @author <a href="mailto:jclasslib@ej-technologies.com">Ingo Kegel</a>
-*/
-public class CountedInputStream extends FilterInputStream {
+ * InputStream which counts the number of bytes read.
 
-    private int bytesRead = 0;
+ * @author [Ingo Kegel](mailto:jclasslib@ej-technologies.com)
+ */
+class CountedInputStream (input: InputStream) : FilterInputStream(input) {
 
     /**
-        Constructor.
-        @param in the input stream.
+     * Number of bytes read.
      */
-    public CountedInputStream(InputStream in) {
-        super(in);
-    }
-    
-    public int read() throws IOException {
-        int b = in.read();
-        //if (b != -1) {
-            bytesRead++;
-        //}
-        return b;
+    var bytesRead = 0
+        private set
+
+    @Throws(IOException::class)
+    override fun read(): Int {
+        val b = `in`.read()
+        bytesRead++
+        return b
     }
 
-    public int read(byte[] b) throws IOException {
-        return read(b, 0, b.length);
+    @Throws(IOException::class)
+    override fun read(b: ByteArray): Int {
+        return read(b, 0, b.size)
     }
 
-    public int read(byte[] b, int offset, int len) throws IOException {
-        int readCount = in.read(b, 0, b.length);
-        bytesRead += readCount;
-        return readCount;
-        
+    @Throws(IOException::class)
+    override fun read(b: ByteArray, offset: Int, len: Int): Int {
+        val readCount = `in`.read(b, 0, b.size)
+        bytesRead += readCount
+        return readCount
+
     }
-    
-    public long skip(long n) throws IOException {
-        long skipCount = in.skip(n);
-        bytesRead += (int)skipCount;
-        return skipCount;
+
+    @Throws(IOException::class)
+    override fun skip(n: Long): Long {
+        val skipCount = `in`.skip(n)
+        bytesRead += skipCount.toInt()
+        return skipCount
     }
 
     // Marking invalidates bytesRead
-    public boolean markSupported() {
-        return false;
-    }
-   
-    /**
-        Get the number of bytes read.
-        @return the number of bytes
-     */
-    public int getBytesRead() {
-        return bytesRead;
+    override fun markSupported(): Boolean {
+        return false
     }
 }
