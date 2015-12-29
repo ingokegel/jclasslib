93d8d9e9f9885fd86e93ec3468a3f6bb9d70afd6
ingo <ingo.kegel@ej-technologies.com>
2015-12-29 22:06:38 +0100

    Log j2k (conversion)

--- modules/data/src/main/java/org/gjt/jclasslib/io/Log.kt
+++ modules/data/src/main/java/org/gjt/jclasslib/io/Log.kt
@@ -5,43 +5,40 @@
     version 2 of the license, or (at your option) any later version.
 */
 
-package org.gjt.jclasslib.io;
+package org.gjt.jclasslib.io
 
 /**
-    Utility class to log errors, warnings and debug messages.
- 
-    @author <a href="mailto:jclasslib@ej-technologies.com">Ingo Kegel</a>
-*/
-public class Log {
+ * Utility class to log errors, warnings and debug messages.
 
-    private Log() {
-    }
+ * @author [Ingo Kegel](mailto:jclasslib@ej-technologies.com)
+ */
+object Log {
 
     /**
-        Log an error message.
-        @param message the message
+     * Log an error message.
+     * @param message the message
      */
-    public static void error(String message) {
-        System.out.print("[error] ");
-        System.out.println(message);
+    fun error(message: String) {
+        print("[error] ")
+        println(message)
     }
 
     /**
-        Log a warning message.
-        @param message the message
+     * Log a warning message.
+     * @param message the message
      */
-    public static void warning(String message) {
-        System.out.print("[warning] ");
-        System.out.println(message);
+    fun warning(message: String) {
+        print("[warning] ")
+        println(message)
     }
-    
+
     /**
-        Log a debug message.
-        @param message the message
+     * Log a debug message.
+     * @param message the message
      */
-    public static void debug(String message) {
-        System.out.print("[debug] ");
-        System.out.println(message);
+    fun debug(message: String) {
+        print("[debug] ")
+        println(message)
     }
-    
+
 }
