d2bd32f25950e5cee62060ad9c0d3ca97a7043d5
ingo <ingo.kegel@ej-technologies.com>
2016-01-17 16:47:48 +0100

    ClasspathComponent j2k (conversion)

--- modules/browser/src/main/java/org/gjt/jclasslib/browser/config/classpath/ClasspathComponent.kt
+++ modules/browser/src/main/java/org/gjt/jclasslib/browser/config/classpath/ClasspathComponent.kt
@@ -5,39 +5,13 @@
     version 2 of the license, or (at your option) any later version.
 */
 
-package org.gjt.jclasslib.browser.config.classpath;
+package org.gjt.jclasslib.browser.config.classpath
 
-/**
-    A logical component of the classpath.
+import javax.swing.tree.DefaultTreeModel
 
-    @author <a href="mailto:jclasslib@ej-technologies.com">Ingo Kegel</a>
-*/
-public interface ClasspathComponent {
-
-    /**
-     * Find a class by name within this classpath component.
-     * @param className the name of the class
-     * @return the <tt>FindResult</tt> object. <tt>null</tt> if no class could be found.
-     */
-    public FindResult findClass(String className);
-
-    /**
-     * Merge all classes in this classpath component into the supplied tree.
-     * @param model the tree model.
-     * @param reset whether this is an incremental operation or part of a reset.
-     *              For a reset, no change events will be fired on the tree model.
-     */
-    public void mergeClassesIntoTree(DefaultTreeModel model, boolean reset);
-
-    /**
-     * Add a <tt>ClasspathChangeListener</tt>.
-     * @param listener the listener
-     */
-    public void addClasspathChangeListener(ClasspathChangeListener listener);
-
-    /**
-     * Remove a <tt>ClasspathChangeListener</tt>.
-     * @param listener the listener
-     */
-    public void removeClasspathChangeListener(ClasspathChangeListener listener);
+interface ClasspathComponent {
+    fun findClass(className: String): FindResult?
+    fun mergeClassesIntoTree(model: DefaultTreeModel, reset: Boolean)
+    fun addClasspathChangeListener(listener: ClasspathChangeListener)
+    fun removeClasspathChangeListener(listener: ClasspathChangeListener)
 }
