df5cee2f99c3d0b15c29b42cf089d538b71d880d
ingo <ingo.kegel@ej-technologies.com>
2016-02-07 13:49:31 +0100

    AnnotationDetailPane j2k (conversion)

--- modules/browser/src/main/java/org/gjt/jclasslib/browser/detail/AnnotationDetailPane.kt
+++ modules/browser/src/main/java/org/gjt/jclasslib/browser/detail/AnnotationDetailPane.kt
@@ -4,51 +4,14 @@
     License as published by the Free Software Foundation; either
     version 2 of the license, or (at your option) any later version.
 */
-package org.gjt.jclasslib.browser.detail;
+package org.gjt.jclasslib.browser.detail
 
-import org.gjt.jclasslib.browser.BrowserServices;
-import org.gjt.jclasslib.browser.BrowserTreeNode;
-import org.gjt.jclasslib.structures.AnnotationData;
-import org.gjt.jclasslib.util.ExtendedJLabel;
+import org.gjt.jclasslib.browser.BrowserServices
+import org.gjt.jclasslib.structures.AnnotationData
 
-import javax.swing.tree.TreePath;
-
-/**
- * Class for showing an annotation node.
- *
- * @author <a href="mailto:vitor.carreira@gmail.com">Vitor Carreira</a>
- *
- */
-public class AnnotationDetailPane extends FixedListDetailPane {
-
-    private ExtendedJLabel lblType;
-    private ExtendedJLabel lblTypeVerbose;
-
-    private ExtendedJLabel lblValuePairEntries;
-
-    public AnnotationDetailPane(BrowserServices services) {
-        super(services);
-    }
-
-    protected void addLabels() {
-
-        addDetailPaneEntry(normalLabel("Type:"),
-                lblType = linkLabel(),
-                lblTypeVerbose = highlightLabel());
-
-        addDetailPaneEntry(normalLabel("Number of entries:"),
-                lblValuePairEntries = highlightLabel());
-    }
-
-    public void show(TreePath treePath) {
-        AnnotationData annotation = (AnnotationData)((BrowserTreeNode)treePath.getLastPathComponent()).getElement();
-
-        constantPoolHyperlink(lblType,
-                lblTypeVerbose,
-                annotation.getTypeIndex());
-
-        lblValuePairEntries.setText(String.valueOf(annotation.getElementValuePairEntries().length));
-
-        super.show(treePath);
+class AnnotationDetailPane(services: BrowserServices) : TypedDetailPane<AnnotationData>(AnnotationData::class.java, services) {
+    override fun addLabels() {
+        addConstantPoolLink("Type:") { annotationData -> annotationData.typeIndex }
+        addDetail("Number of entries:") { annotationData -> annotationData.elementValuePairEntries.size.toString() }
     }
 }
