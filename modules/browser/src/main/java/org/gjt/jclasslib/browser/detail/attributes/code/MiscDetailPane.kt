28e2e2cb5f0d508690d62ee56783e81104763cd0
ingo <ingo.kegel@ej-technologies.com>
2016-02-07 13:36:28 +0100

    MiscDetailPane j2k (conversion)

--- modules/browser/src/main/java/org/gjt/jclasslib/browser/detail/attributes/code/MiscDetailPane.kt
+++ modules/browser/src/main/java/org/gjt/jclasslib/browser/detail/attributes/code/MiscDetailPane.kt
@@ -5,60 +5,21 @@
     version 2 of the license, or (at your option) any later version.
 */
 
-package org.gjt.jclasslib.browser.detail.attributes.code;
+package org.gjt.jclasslib.browser.detail.attributes.code
 
-import org.gjt.jclasslib.browser.BrowserServices;
-import org.gjt.jclasslib.browser.detail.FixedListDetailPane;
-import org.gjt.jclasslib.structures.attributes.CodeAttribute;
-import org.gjt.jclasslib.util.ExtendedJLabel;
+import org.gjt.jclasslib.browser.BrowserServices
+import org.gjt.jclasslib.browser.detail.TypedDetailPane
+import org.gjt.jclasslib.structures.attributes.CodeAttribute
 
-import javax.swing.tree.TreePath;
-
-/**
-    Detail pane showing miscellaneous information of a <tt>Code</tt> attribute
-    without substructure.
- 
-    @author <a href="mailto:jclasslib@ej-technologies.com">Ingo Kegel</a>
-*/
-public class MiscDetailPane extends FixedListDetailPane {
-
-    // Visual components
-    
-    private ExtendedJLabel lblMaxStack;
-    private ExtendedJLabel lblMaxLocals;
-    private ExtendedJLabel lblCodeLength;
-    
-    /**
-        Constructor.
-        @param services the associated browser services.
-     */
-    public MiscDetailPane(BrowserServices services) {
-        super(services);
-        setName("Misc");
-    }
-    
-    protected void addLabels() {
-        
-        addDetailPaneEntry(normalLabel("Maximum stack depth:"),
-                           lblMaxStack = highlightLabel());
-
-        addDetailPaneEntry(normalLabel("Maximum local variables:"),
-                           lblMaxLocals = highlightLabel());
-
-        addDetailPaneEntry(normalLabel("Code length:"),
-                           lblCodeLength = highlightLabel());
+class MiscDetailPane(services: BrowserServices) : TypedDetailPane<CodeAttribute>(CodeAttribute::class.java, services) {
+    init {
+        name = "Misc"
     }
 
-    public void show(TreePath treePath) {
-        
-        CodeAttribute attribute = (CodeAttribute)getAttribute(treePath);
-
-        lblMaxStack.setText(attribute.getMaxStack());
-        lblMaxLocals.setText(attribute.getMaxLocals());
-        lblCodeLength.setText(attribute.getCode().length);
-        
-        super.show(treePath);
+    override fun addLabels() {
+        addDetail("Minor version:") { attribute -> attribute.maxStack.toString() }
+        addDetail("Maximum local variables:") { attribute -> attribute.maxLocals.toString() }
+        addDetail("Code length:") { attribute -> attribute.code.size.toString() }
     }
-    
 }
 
