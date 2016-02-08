c3c46913cd1ebedfb7313bb15c307404a18f5219
ingo <ingo.kegel@ej-technologies.com>
2016-02-08 22:19:42 +0100

    ConstantValueAttributeDetailPane j2k (conversion)

--- modules/browser/src/main/java/org/gjt/jclasslib/browser/detail/attributes/ConstantValueAttributeDetailPane.kt
+++ modules/browser/src/main/java/org/gjt/jclasslib/browser/detail/attributes/ConstantValueAttributeDetailPane.kt
@@ -5,52 +5,15 @@
     version 2 of the license, or (at your option) any later version.
 */
 
-package org.gjt.jclasslib.browser.detail.attributes;
+package org.gjt.jclasslib.browser.detail.attributes
 
-import org.gjt.jclasslib.browser.BrowserServices;
-import org.gjt.jclasslib.browser.detail.FixedListDetailPane;
-import org.gjt.jclasslib.structures.attributes.ConstantValueAttribute;
-import org.gjt.jclasslib.util.ExtendedJLabel;
+import org.gjt.jclasslib.browser.BrowserServices
+import org.gjt.jclasslib.browser.detail.TypedDetailPane
+import org.gjt.jclasslib.structures.attributes.ConstantValueAttribute
 
-import javax.swing.tree.TreePath;
-
-/**
-    Detail pane showing a <tt>ConstantValue</tt> attribute.
- 
-    @author <a href="mailto:jclasslib@ej-technologies.com">Ingo Kegel</a>
-*/
-public class ConstantValueAttributeDetailPane extends FixedListDetailPane {
-
-    // Visual components
-    
-    private ExtendedJLabel lblValue;
-    private ExtendedJLabel lblVerbose;
-    
-    /**
-        Constructor.
-        @param services the associated browser services.
-     */
-    public ConstantValueAttributeDetailPane(BrowserServices services) {
-        super(services);
+class ConstantValueAttributeDetailPane(services: BrowserServices) : TypedDetailPane<ConstantValueAttribute>(ConstantValueAttribute::class.java, services) {
+    override fun addLabels() {
+        addConstantPoolLink("Constant value index:") { attribute -> attribute.constantValueIndex }
     }
-    
-    protected void addLabels() {
-        
-        addDetailPaneEntry(normalLabel("Constant value index:"),
-                           lblValue = linkLabel(),
-                           lblVerbose = highlightLabel());
-    }
-
-    public void show(TreePath treePath) {
-        
-        ConstantValueAttribute attribute = (ConstantValueAttribute)getAttribute(treePath);
-
-        constantPoolHyperlink(lblValue,
-                              lblVerbose,
-                              attribute.getConstantValueIndex());
-        
-        super.show(treePath);
-    }
-    
 }
 
