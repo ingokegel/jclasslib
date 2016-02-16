32d3f112ebb1e7cb6261fc1c7e6c7029c988f639
ingo <ingo.kegel@ej-technologies.com>
2016-02-16 22:25:05 +0100

    ClassFileConsistencyTest j2k (conversion)

--- modules/data/src/test/java/org/gjt/jclasslib/test/ClassFileConsistencyTest.kt
+++ modules/data/src/test/java/org/gjt/jclasslib/test/ClassFileConsistencyTest.kt
@@ -5,142 +5,104 @@
  version 2 of the license, or (at your option) any later version.
  */
 
-package org.gjt.jclasslib.test;
+package org.gjt.jclasslib.test
 
 import org.gjt.jclasslib.io.ClassFileReader
-import org.gjt.jclasslib.structures.ClassFile
+import org.gjt.jclasslib.io.ClassFileWriter
+import org.gjt.jclasslib.structures.isDebug
+import org.testng.annotations.Test
+import java.io.ByteArrayInputStream
 import java.io.ByteArrayOutputStream
-import java.io.IOException
+import java.io.File
 import java.io.InputStream
 import java.net.URL
 import java.util.jar.JarEntry
 import java.util.jar.JarFile
 
-public abstract class ClassFileConsistencyTest {
-
-    private static final ThreadLocal<byte[]> BUFFER_TL = new ThreadLocal<byte[]>() {
-        @Override
-        protected byte[] initialValue() {
-            return new byte[1024];
-        }
-    };
-
-    protected void scanJar(File file) throws IOException, InvalidByteCodeException {
-        JarFile jar = new JarFile(file);
-        Enumeration<JarEntry> entries = jar.entries();
-        int count = 0;
-        int errors = 0;
-        while (entries.hasMoreElements()) {
-            JarEntry entry = entries.nextElement();
-            String name = entry.getName();
-            int index = name.lastIndexOf(".class");
-            if (index > 0) {
-                String className = name.substring(0, index).replace("/", ".");
-                try {
-                    if (!checkClassFile(className, new JarInputStreamProvider(jar, entry))) {
-                        errors++;
-                    }
-                } catch (IOException e) {
-                    error(className);
-                    throw e;
-                } catch (InvalidByteCodeException e) {
-                    error(className);
-                    throw e;
-                } catch (RuntimeException e) {
-                    error(className);
-                    throw e;
-                }
-                count++;
-            }
-        }
-        System.out.println(String.valueOf(count) + " classes checked, " + errors + " errors");
+class Tests {
+    @Test
+    fun testCurrentJre() {
+        scanJar(File("${System.getProperty("java.home")}/lib/rt.jar"))
     }
 
-    protected static boolean checkClassFile(String className, URL url) throws IOException, InvalidByteCodeException {
-        return checkClassFile(className, new UrlInputStreamProvider(url));
-    }
-
-    protected static boolean checkClassFile(String className, InputStreamProvider inputStreamProvider) throws IOException, InvalidByteCodeException {
-
-        ByteArrayOutputStream os = new ByteArrayOutputStream();
-        InputStream is = inputStreamProvider.createInputStream();
-        pumpStream(is, os);
-        is.close();
-        byte[] before = os.toByteArray();
-
-        ClassFile classFile = ClassFileReader.readFromInputStream(inputStreamProvider.createInputStream());
-        byte[] after = ClassFileWriter.writeToByteArray(classFile);
-
-        boolean success = compare(className, before, after);
-        if (!success) {
-            Package.setDebug(true);
-            ClassFileReader.readFromInputStream(new ByteArrayInputStream(after));
-        }
-        return success;
-    }
-
-    private static void error(String className) {
-        System.err.println("ERROR when processing " + className);
-    }
-
-    private static boolean compare(String className, byte[] before, byte[] after) {
-        if (before.length != after.length) {
-            System.err.println("ERROR in " + className);
-            System.err.println("Different length " + before.length + " != " + after.length);
-            return false;
-        }
-
-        for (int i = 0; i < before.length; i++) {
-            if (before[i] != after[i]) {
-                System.err.println("Different byte at index " + i);
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    private static long pumpStream(InputStream is, OutputStream os) throws IOException {
-
-        byte[] buffer = BUFFER_TL.get();
-        int count;
-        while ((count = is.read(buffer)) != -1) {
-            os.write(buffer, 0, count);
-        }
-        return count;
-    }
-
-    protected interface InputStreamProvider {
-        InputStream createInputStream() throws IOException;
-    }
-
-    protected static class JarInputStreamProvider implements InputStreamProvider {
-
-        private JarFile jarFile;
-        private JarEntry jarEntry;
-
-        public JarInputStreamProvider(JarFile jarFile, JarEntry jarEntry) {
-            this.jarFile = jarFile;
-            this.jarEntry = jarEntry;
-        }
-
-        @Override
-        public InputStream createInputStream() throws IOException {
-            return jarFile.getInputStream(jarEntry);
-        }
-    }
-
-    protected static class UrlInputStreamProvider implements InputStreamProvider {
-
-        private URL url;
-
-        public UrlInputStreamProvider(URL url) {
-            this.url = url;
-        }
-
-        @Override
-        public InputStream createInputStream() throws IOException {
-            return url.openConnection().getInputStream();
-        }
+    @Test
+    fun test() {
+        checkClassFile("Main", InputStreamProvider::class.java.getResource("/java8/Main.class"))
     }
 }
+
+fun scanJar(file: File) {
+    val jar = JarFile(file)
+    var count = 0
+    var errors = 0
+    jar.entries().iterator().forEach { entry ->
+        val name = entry.name
+        if (name.endsWith(".class")) {
+            val className = name.removeSuffix(".class").replace("/", ".")
+            try {
+                if (!checkClassFile(className, JarInputStreamProvider(jar, entry))) {
+                    errors++
+                }
+            } catch (e: Throwable) {
+                error(className)
+                throw e
+            }
+            count++
+        }
+    }
+    println(count.toString() + " classes checked, " + errors + " errors")
+}
+
+interface InputStreamProvider {
+    fun createInputStream(): InputStream
+}
+
+class JarInputStreamProvider(private val jarFile: JarFile, private val jarEntry: JarEntry) : InputStreamProvider {
+    override fun createInputStream() = jarFile.getInputStream(jarEntry)
+}
+
+class UrlInputStreamProvider(private val url: URL) : InputStreamProvider {
+    override fun createInputStream() = url.openConnection().inputStream
+}
+
+fun checkClassFile(className: String, url: URL): Boolean {
+    return checkClassFile(className, UrlInputStreamProvider(url))
+}
+
+fun checkClassFile(className: String, inputStreamProvider: InputStreamProvider): Boolean {
+    val output = ByteArrayOutputStream()
+    inputStreamProvider.createInputStream().use {
+        it.copyTo(output)
+    }
+    val before = output.toByteArray()
+
+    val classFile = ClassFileReader.readFromInputStream(inputStreamProvider.createInputStream())
+    val after = ClassFileWriter.writeToByteArray(classFile)
+
+    val success = compare(className, before, after)
+    if (!success) {
+        isDebug = true
+        ClassFileReader.readFromInputStream(ByteArrayInputStream(after))
+    }
+    return success
+}
+
+fun error(className: String) {
+    System.err.println("ERROR when processing " + className)
+}
+
+fun compare(className: String, before: ByteArray, after: ByteArray): Boolean {
+    if (before.size != after.size) {
+        System.err.println("ERROR in " + className)
+        System.err.println("Different length " + before.size + " != " + after.size)
+        return false
+    }
+    for (i in before.indices) {
+        if (before[i] != after[i]) {
+            System.err.println("Different byte at index " + i)
+            return false
+        }
+    }
+    return true
+}
+
