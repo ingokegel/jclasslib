bc505ad1a5bf0af4f92f3ac1c81533ebaa85b787
ingo <ingo.kegel@ej-technologies.com>
2016-02-16 22:03:02 +0100

    ClassFileConsistencyTest j2k (rename)

--- modules/data/src/test/java/org/gjt/jclasslib/test/ClassFileConsistencyTest.kt
+++ modules/data/src/test/java/org/gjt/jclasslib/test/ClassFileConsistencyTest.kt
@@ -7,17 +7,14 @@
 
 package org.gjt.jclasslib.test;
 
-import org.gjt.jclasslib.io.ClassFileReader;
-import org.gjt.jclasslib.io.ClassFileWriter;
-import org.gjt.jclasslib.structures.ClassFile;
-import org.gjt.jclasslib.structures.InvalidByteCodeException;
-import org.gjt.jclasslib.structures.Package;
-
-import java.io.*;
-import java.net.URL;
-import java.util.Enumeration;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
+import org.gjt.jclasslib.io.ClassFileReader
+import org.gjt.jclasslib.structures.ClassFile
+import java.io.ByteArrayOutputStream
+import java.io.IOException
+import java.io.InputStream
+import java.net.URL
+import java.util.jar.JarEntry
+import java.util.jar.JarFile
 
 public abstract class ClassFileConsistencyTest {
 
