1eac367826f437b9e47dbf3df129bf358e428aeb
Ingo Kegel <ingo.kegel@ej-technologies.com>
2016-06-06 12:48:46 +0200

    improved dragging of tabs

--- modules/browser/src/main/kotlin/org/gjt/jclasslib/util/DnDTabbedPane.kt
+++ modules/browser/src/main/kotlin/org/gjt/jclasslib/util/DnDTabbedPane.kt
@@ -13,12 +13,12 @@ import java.awt.datatransfer.Transferable
 import java.awt.dnd.*
 import java.awt.geom.Rectangle2D
 import java.awt.image.BufferedImage
-import javax.swing.JComponent
-import javax.swing.JPanel
-import javax.swing.JTabbedPane
-import javax.swing.SwingUtilities
+import java.util.*
+import javax.swing.*
 
 open class DnDTabbedPane : JTabbedPane() {
+
+    private val glassPane : GhostGlassPane by lazy { createOrGetGlassPane(rootPane) }
 
     val tabAcceptor: TabAcceptor = object : TabAcceptor {
         override fun isDropAcceptable(tabbedPane: DnDTabbedPane, index: Int): Boolean {
@@ -26,12 +26,10 @@ open class DnDTabbedPane : JTabbedPane() {
         }
     }
 
-    private var isDrawRect = false
-    private val line = Rectangle2D.Double()
-
     init {
         addDropTarget(this)
         DragSource().createDefaultDragGestureRecognizer(this, DnDConstants.ACTION_COPY_OR_MOVE, createDragGestureListener())
+        tabLayoutPolicy = SCROLL_TAB_LAYOUT
     }
 
     fun addDropTarget(component: JComponent, dropNotifier: () -> Unit = {}) {
@@ -39,33 +37,28 @@ open class DnDTabbedPane : JTabbedPane() {
     }
 
     private fun createDragGestureListener() = DragGestureListener { event ->
-        val tabPt = event.dragOrigin
-        val dragTabIndex = indexAtLocation(tabPt.x, tabPt.y)
-        if (dragTabIndex < 0) {
-            return@DragGestureListener
-        }
-
-        initGlassPane(event.component, event.dragOrigin, dragTabIndex)
-        try {
-            event.startDrag(DragSource.DefaultMoveDrop, TabTransferable(dragTabIndex), createDragSourceListener())
-        } catch (e: InvalidDnDOperationException) {
-            e.printStackTrace()
+        val tabLocation = event.dragOrigin
+        val dragTabIndex = indexAtLocation(tabLocation.x, tabLocation.y)
+        if (dragTabIndex >= 0) {
+            initGlassPane(event.component, event.dragOrigin, dragTabIndex)
+            try {
+                event.startDrag(DragSource.DefaultMoveDrop, TabTransferable(dragTabIndex), createDragSourceListener())
+            } catch (e: InvalidDnDOperationException) {
+                e.printStackTrace()
+            }
         }
     }
 
     private fun createDragSourceListener() = object : DragSourceListener {
         override fun dragEnter(event: DragSourceDragEvent) {
             event.dragSourceContext.cursor = DragSource.DefaultMoveDrop
+            drawDrag = true
         }
 
         override fun dragExit(event: DragSourceEvent) {
             event.dragSourceContext.cursor = DragSource.DefaultMoveNoDrop
-            line.origin()
-            isDrawRect = false
-            GLASS_PANE.apply {
-                setPoint(Point(-1000, -1000))
-                repaint()
-            }
+            drawDrag = false
+            repaintGlassPanes()
         }
 
         override fun dragOver(event: DragSourceDragEvent) {
@@ -78,12 +71,7 @@ open class DnDTabbedPane : JTabbedPane() {
         }
 
         override fun dragDropEnd(event: DragSourceDropEvent) {
-            isDrawRect = false
-            line.origin()
-            GLASS_PANE.apply {
-                isVisible = false
-                setImage(null)
-            }
+            hideGlassPanes()
         }
 
         override fun dropActionChanged(event: DragSourceDragEvent) {
@@ -99,31 +87,28 @@ open class DnDTabbedPane : JTabbedPane() {
     private fun getTabTransferData(event: DragSourceDragEvent) =
             event.dragSourceContext.transferable.getTransferData(FLAVOR) as TabTransferData?
 
-    private fun buildGhostLocation(location: Point): Point {
-        val copy = Point(location).apply {
-            when (getTabPlacement()) {
-                JTabbedPane.TOP -> {
-                    y = 1
-                    x -= GLASS_PANE.ghostWidth / 2
-                }
+    private fun buildGhostLocation(location: Point) = Point(location).apply {
+        when (getTabPlacement()) {
+            JTabbedPane.TOP -> {
+                y = 1
+                x -= glassPane.ghostWidth / 2
+            }
 
-                JTabbedPane.BOTTOM -> {
-                    y = height - 1 - GLASS_PANE.ghostHeight
-                    x -= GLASS_PANE.ghostWidth / 2
-                }
+            JTabbedPane.BOTTOM -> {
+                y = height - 1 - glassPane.ghostHeight
+                x -= glassPane.ghostWidth / 2
+            }
 
-                JTabbedPane.LEFT -> {
-                    x = 1
-                    y -= GLASS_PANE.ghostHeight / 2
-                }
+            JTabbedPane.LEFT -> {
+                x = 1
+                y -= glassPane.ghostHeight / 2
+            }
 
-                JTabbedPane.RIGHT -> {
-                    x = width - 1 - GLASS_PANE.ghostWidth
-                    y -= GLASS_PANE.ghostHeight / 2
-                }
+            JTabbedPane.RIGHT -> {
+                x = width - 1 - glassPane.ghostWidth
+                y -= glassPane.ghostHeight / 2
             }
         }
-        return SwingUtilities.convertPoint(this@DnDTabbedPane, copy, GLASS_PANE)
     }
 
     private fun getTargetTabIndex(point: Point): Int {
@@ -136,9 +121,9 @@ open class DnDTabbedPane : JTabbedPane() {
         for (i in 0..tabCount - 1) {
             val r = getBoundsAt(i) ?: continue
             if (isTopOrBottom) {
-                r.setRect(r.x - r.width / 2, r.y, r.width, r.height)
+                r.setRect(r.x - r.width / 2, r.y, r.width, height)
             } else {
-                r.setRect(r.x, r.y - r.height / 2, r.width, r.height)
+                r.setRect(r.x, r.y - r.height / 2, width, r.height)
             }
 
             if (r.contains(point)) {
@@ -149,10 +134,10 @@ open class DnDTabbedPane : JTabbedPane() {
         val r = getLastVisibleTabBound() ?: return -1
         if (isTopOrBottom) {
             val x = r.x + r.width / 2
-            r.setRect(x, r.y, width - x, r.height)
+            r.setRect(x, r.y, width - x, height)
         } else {
             val y = r.y + r.height / 2
-            r.setRect(r.x, y, r.width, height - y)
+            r.setRect(r.x, y, width, height - y)
         }
 
         return if (r.contains(point)) tabCount else -1
@@ -162,14 +147,14 @@ open class DnDTabbedPane : JTabbedPane() {
         return getNextVisibleTabBound(-1)
     }
 
-    private fun getNextVisibleTabBound(index : Int): Rectangle? {
+    private fun getNextVisibleTabBound(index: Int): Rectangle? {
         for (i in index + 1..tabCount - 1) {
             return getBoundsAt(i) ?: continue
         }
         return null
     }
 
-    private fun getPreviousVisibleTabBound(index : Int): Rectangle? {
+    private fun getPreviousVisibleTabBound(index: Int): Rectangle? {
         for (i in index - 1 downTo 0) {
             return getBoundsAt(i) ?: continue
         }
@@ -227,66 +212,48 @@ open class DnDTabbedPane : JTabbedPane() {
         }
     }
 
-    private fun initTargetLeftRightLine(next: Int, data: TabTransferData) {
-        if (next < 0) {
-            line.origin()
-            isDrawRect = false
-            return
-        }
-
-        if (data.tabbedPane === this && (data.tabIndex == next || next - data.tabIndex == 1)) {
-            line.origin()
-            isDrawRect = false
+    private fun initTargetLeftRightLine(next: Int, data: TabTransferData) : Boolean {
+        return if (next < 0) {
+            false
+        } else if (data.tabbedPane === this && (data.tabIndex == next || next - data.tabIndex == 1)) {
+            false
         } else if (tabCount == 0) {
-            line.origin()
-            isDrawRect = false
-            return
+            false
         } else if (next == 0) {
-            val rect = getFirstVisibleTabBound() ?: return
-            line.setRect(rect.x - LINE_WIDTH / 2, rect.y, LINE_WIDTH, rect.height)
-            isDrawRect = true
+            val rect = getFirstVisibleTabBound() ?: return false
+            glassPane.insertionMarker.setRect(rect.x - LINE_WIDTH / 2, rect.y, LINE_WIDTH, rect.height)
+            true
         } else if (next == tabCount) {
-            val rect = getLastVisibleTabBound() ?: return
-            line.setRect(rect.x + rect.width - LINE_WIDTH / 2, rect.y, LINE_WIDTH, rect.height)
-            isDrawRect = true
+            val rect = getLastVisibleTabBound() ?: return false
+            glassPane.insertionMarker.setRect(rect.x + rect.width - LINE_WIDTH / 2, rect.y, LINE_WIDTH, rect.height)
+            true
         } else {
-            val rect = getPreviousVisibleTabBound(next) ?: return
-            line.setRect(rect.x + rect.width - LINE_WIDTH / 2, rect.y, LINE_WIDTH, rect.height)
-            isDrawRect = true
+            val rect = getPreviousVisibleTabBound(next) ?: return false
+            glassPane.insertionMarker.setRect(rect.x + rect.width - LINE_WIDTH / 2, rect.y, LINE_WIDTH, rect.height)
+            true
         }
     }
 
-    private fun initTargetTopBottomLine(next: Int, data: TabTransferData) {
-        if (next < 0) {
-            line.origin()
-            isDrawRect = false
-            return
-        }
-
-        if (data.tabbedPane === this && (data.tabIndex == next || next - data.tabIndex == 1)) {
-            line.origin()
-            isDrawRect = false
+    private fun initTargetTopBottomLine(next: Int, data: TabTransferData) : Boolean {
+        return if (next < 0) {
+            false
+        } else if (data.tabbedPane === this && (data.tabIndex == next || next - data.tabIndex == 1)) {
+            false
         } else if (tabCount == 0) {
-            line.origin()
-            isDrawRect = false
-            return
+            false
         } else if (next == tabCount) {
-            val rect = getLastVisibleTabBound() ?: return
-            line.setRect(rect.x, rect.y + rect.height - LINE_WIDTH / 2, rect.width, LINE_WIDTH)
-            isDrawRect = true
+            val rect = getLastVisibleTabBound() ?: return false
+            glassPane.insertionMarker.setRect(rect.x, rect.y + rect.height - LINE_WIDTH / 2, rect.width, LINE_WIDTH)
+            true
         } else if (next == 0) {
-            val rect = getFirstVisibleTabBound() ?: return
-            line.setRect(rect.x, -LINE_WIDTH / 2, rect.width, LINE_WIDTH)
-            isDrawRect = true
+            val rect = getFirstVisibleTabBound() ?: return false
+            glassPane.insertionMarker.setRect(rect.x, -LINE_WIDTH / 2, rect.width, LINE_WIDTH)
+            true
         } else {
-            val rect = getPreviousVisibleTabBound(next) ?: return
-            line.setRect(rect.x, rect.y + rect.height - LINE_WIDTH / 2, rect.width, LINE_WIDTH)
-            isDrawRect = true
+            val rect = getPreviousVisibleTabBound(next) ?: return false
+            glassPane.insertionMarker.setRect(rect.x, rect.y + rect.height - LINE_WIDTH / 2, rect.width, LINE_WIDTH)
+            true
         }
-    }
-
-    fun Rectangle2D.Double.origin() {
-        this.setRect(0, 0, 0, 0)
     }
 
     fun Rectangle2D.Double.setRect(x: Int, y: Int, width: Int, height: Int) {
@@ -297,15 +264,22 @@ open class DnDTabbedPane : JTabbedPane() {
         this.setRect(x.toDouble(), y.toDouble(), width.toDouble(), height.toDouble())
     }
 
-    private fun initGlassPane(c: Component, tabPt: Point, tabIndex: Int) {
-        rootPane.glassPane = GLASS_PANE
+    private fun initGlassPane(c: Component, tabLocation: Point, tabIndex: Int) {
         val image = BufferedImage(c.width, c.height, BufferedImage.TYPE_INT_ARGB)
         c.paint(image.graphics)
-        GLASS_PANE.apply {
+        updateGlassPane(tabLocation)
+        glassPane.apply {
             val rect = getBoundsAt(tabIndex) ?: return
-            setImage(image.getSubimage(rect.x, rect.y, rect.width, rect.height))
-            setPoint(buildGhostLocation(tabPt))
+            draggingGhost = image.getSubimage(rect.x, rect.y, rect.width, rect.height)
+        }
+    }
+
+    private fun updateGlassPane(point: Point) {
+        glassPane.apply {
+            ghostLocation = buildGhostLocation(point)
+            glassPane.offset = SwingUtilities.convertPoint(this@DnDTabbedPane, 0, 0, this)
             isVisible = true
+            drawDrag = true
         }
     }
 
@@ -313,35 +287,15 @@ open class DnDTabbedPane : JTabbedPane() {
     protected open fun handleDrop(event: DropTargetDropEvent) {
     }
 
-    public override fun paintComponent(g: Graphics) {
-        super.paintComponent(g)
-        if (isDrawRect) {
-            (g as Graphics2D).apply {
-                paint = LINE_COLOR
-                fill(line)
-            }
-        }
-    }
-
     private class GhostGlassPane : JPanel() {
-        private val composite: AlphaComposite
-        private val point = Point(0, 0)
-        private var draggingGhost: BufferedImage? = null
+        var offset = Point(0, 0)
+        var ghostLocation = Point(0, 0)
+        val insertionMarker = Rectangle2D.Double()
+        var drawInsertionMarker = false
 
         init {
             isOpaque = false
-            composite = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.7f)
-        }
-
-        fun setImage(draggingGhost: BufferedImage?) {
-            this.draggingGhost = draggingGhost
-        }
-
-        fun setPoint(point: Point) {
-            this.point.apply {
-                x = point.x
-                y = point.y
-            }
+            isVisible = false
         }
 
         val ghostWidth: Int
@@ -351,12 +305,19 @@ open class DnDTabbedPane : JTabbedPane() {
             get() = draggingGhost?.getHeight(this) ?: 0
 
         public override fun paintComponent(g: Graphics) {
-            if (draggingGhost == null) {
+            if (draggingGhost == null || !drawDrag) {
                 return
             }
-            val g2 = g as Graphics2D
-            g2.composite = composite
-            g2.drawImage(draggingGhost, point.getX().toInt(), point.getY().toInt(), null)
+            (g as Graphics2D).apply {
+                translate(offset.x, offset.y)
+                if (drawInsertionMarker) {
+                    paint = INSERTION_MARKER_COLOR
+                    fill(insertionMarker)
+                }
+                composite = GHOST_COMPOSITE
+                drawImage(draggingGhost, ghostLocation.x, ghostLocation.y, null)
+                translate(-offset.x, -offset.y)
+            }
         }
     }
 
@@ -379,7 +340,7 @@ open class DnDTabbedPane : JTabbedPane() {
         }
 
         override fun dragExit(event: DropTargetEvent) {
-            isDrawRect = false
+            glassPane.drawInsertionMarker = false
         }
 
         override fun dropActionChanged(event: DropTargetDragEvent) {
@@ -388,14 +349,14 @@ open class DnDTabbedPane : JTabbedPane() {
         override fun dragOver(event: DropTargetDragEvent) {
             if (isTabTransfer(event.transferable)) {
                 val data = getTabTransferData(event.transferable)
-                if (getTabPlacement() == JTabbedPane.TOP || getTabPlacement() == JTabbedPane.BOTTOM) {
+                glassPane.drawInsertionMarker = if (getTabPlacement() == JTabbedPane.TOP || getTabPlacement() == JTabbedPane.BOTTOM) {
                     initTargetLeftRightLine(getTargetTabIndex(event.location), data)
                 } else {
                     initTargetTopBottomLine(getTargetTabIndex(event.location), data)
                 }
                 repaint()
-                GLASS_PANE.setPoint(buildGhostLocation(event.location))
-                GLASS_PANE.repaint()
+                updateGlassPane(event.location)
+                repaintGlassPanes()
             }
         }
 
@@ -410,8 +371,8 @@ open class DnDTabbedPane : JTabbedPane() {
             } else {
                 event.dropComplete(false)
             }
-            isDrawRect = false
-            repaint()
+            glassPane.drawInsertionMarker = false
+            repaintGlassPanes()
         }
 
         private fun isDropAcceptable(transferable: Transferable?): Boolean {
@@ -446,8 +407,31 @@ open class DnDTabbedPane : JTabbedPane() {
         private val LINE_WIDTH = 3
         private val NAME = "TabTransferData"
         private val FLAVOR = DataFlavor(DataFlavor.javaJVMLocalObjectMimeType, NAME)
-        private val GLASS_PANE = GhostGlassPane()
-        private val LINE_COLOR = Color(0, 100, 255)
+        private val INSERTION_MARKER_COLOR = Color(0, 100, 255)
+        private val GHOST_COMPOSITE = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.7f)
+
+        private val glassPanes = WeakHashMap<JRootPane, GhostGlassPane>()
+        var draggingGhost: BufferedImage? = null
+        var drawDrag = false
+
+        fun repaintGlassPanes() {
+            glassPanes.values.forEach {
+                it.repaint()
+            }
+        }
+
+        fun hideGlassPanes() {
+            glassPanes.values.forEach {
+                it.isVisible = false
+            }
+        }
+
+        private fun createOrGetGlassPane(rootPane: JRootPane) = glassPanes.getOrPut(rootPane) {
+            GhostGlassPane().apply {
+                rootPane.glassPane = this
+            }
+        }
+
     }
 }
 
