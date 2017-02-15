2a1891763343af3fa742cdc9ead6ed021f4636ab
ingo <ingo.kegel@ej-technologies.com>
2017-02-15 17:44:39 +0100

    Converted to gradle script kotlin

--- modules/idea/idea.gradle.kts
+++ modules/idea/idea.gradle.kts
@@ -1,31 +1,43 @@
+import org.jetbrains.intellij.IntelliJPluginExtension
+import org.jetbrains.intellij.tasks.PublishTask
+
 plugins {
-  id "org.jetbrains.intellij" version "0.2.13"
+    id("org.jetbrains.intellij") version "0.2.13"
 }
 
-apply plugin: 'kotlin'
-
-intellij {
-    version 'IC-2016.2.5'
-    pluginName 'jclasslib'
-    // See https://plugins.jetbrains.com/plugin/6954-kotlin for kotlin plugin versions, add channel with @EAP-1.1 at the end of the plugin ID
-    plugins 'ByteCodeViewer', "org.jetbrains.kotlin:$kotlinVersionMain-release-IJ2016.2${kotlinVersionHotfix}"
-    sandboxDirectory "$rootProject.buildDir/../idea_sandbox"
-    updateSinceUntilBuild false
-
-    publish {
-        username=project.findProperty("intellij.publish.username") ?: ''
-        password=project.findProperty("intellij.publish.password") ?: ''
+buildscript {
+    dependencies {
+        repositories {
+            maven {
+                setUrl("http://dl.bintray.com/jetbrains/intellij-plugin-service")
+            }
+        }
     }
 }
 
-dependencies {
-    compile project(':browser')
-    // explicit Kotlin dependency to prevent the intellij plugin from adding the Kotlin libraries in the lib directory
-    compile "org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion"
+apply {
+    plugin("kotlin")
 }
 
-clean {
-    doLast {
-        delete file("${intellij.sandboxDirectory}/plugins")
+val kotlinVersionMain = rootProject.extra["kotlinVersionMain"]
+val kotlinVersionHotfix = rootProject.extra["kotlinVersionHotfix"]
+dependencies {
+    compile(project(":browser"))
+    // explicit Kotlin dependency to prevent the intellij plugin from adding the Kotlin libraries in the lib directory
+    compile("org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion")
+}
+
+configure<IntelliJPluginExtension> {
+    version = "IC-2016.2.5"
+    pluginName = "jclasslib"
+    setPlugins("ByteCodeViewer", "org.jetbrains.kotlin:$kotlinVersionMain-release-IJ2016.2${kotlinVersionHotfix}")
+    sandboxDirectory = "${rootProject.buildDir}/../idea_sandbox"
+    updateSinceUntilBuild = false
+}
+
+tasks {
+    "publishPlugin"(PublishTask::class) {
+        setUsername(project.findProperty("intellij.publish.username") ?: "")
+        setPassword(project.findProperty("intellij.publish.password") ?: "")
     }
 }
