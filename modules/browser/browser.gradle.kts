07ac89c4d4b78433379342f55a7a8d1ca70f97da
ingo <ingo.kegel@ej-technologies.com>
2017-02-15 18:41:33 +0100

    Converted to gradle script kotlin

--- modules/browser/browser.gradle.kts
+++ modules/browser/browser.gradle.kts
@@ -1,34 +1,47 @@
-apply plugin: 'kotlin'
-apply plugin: 'application'
+import org.gradle.api.java.archives.Manifest
+import org.gradle.api.tasks.Copy
+import org.gradle.jvm.tasks.Jar
 
-apply from: file('../publish.gradle')
+plugins {
+    application
+}
 
-mainClassName = 'org.gjt.jclasslib.browser.BrowserApplication'
+apply {
+    plugin("kotlin")
+    from(file("../publish.gradle"))
+}
+
+configure<ApplicationPluginConvention>() {
+    mainClassName = "org.gjt.jclasslib.browser.BrowserApplication"
+}
 
 dependencies {
-    compileOnly ':apple'
-    compile 'com.install4j:install4j-runtime:6.1.4'
-    compile 'org.jetbrains:annotations:13.0'
-    compile 'org.jetbrains.kotlinx:kotlinx.dom:0.0.10'
-    compile 'com.miglayout:miglayout-swing:5.0'
-    compile project(':data')
+    compileOnly(":apple")
+    compile("com.install4j:install4j-runtime:6.1.4")
+    compile("org.jetbrains:annotations:13.0")
+    compile("org.jetbrains.kotlinx:kotlinx.dom:0.0.10")
+    compile("com.miglayout:miglayout-swing:5.0")
+    compile(project(":data"))
 }
 
-jar {
-    archiveName = 'jclasslib-browser.jar'
-    manifest {
-        attributes("Main-Class": mainClassName)
+tasks {
+    val jar: Jar by tasks
+    jar.apply {
+        archiveName = "jclasslib-browser.jar"
+        manifest(closureOf<Manifest> {
+            attributes(mapOf("Main-Class" to the<ApplicationPluginConvention>().mainClassName))
+        })
+    }
+
+    val copyDist by creating(Copy::class) {
+        dependsOn("jar")
+        from(configurations.compile.files.filterNot { it.name.contains("install4j") })
+        from(jar.archivePath)
+        into(rootProject.extra["externalLibsDir"])
+    }
+
+    "dist" {
+        dependsOn(copyDist)
     }
 }
-
-task copyDist(type: Copy, dependsOn: jar) {
-    from configurations.compile.files.findAll {!it.name.contains('install4j')}
-    from jar.archivePath
-    into externalLibsDir
-}
-
-task dist {
-    dependsOn copyDist
-}
-
 
