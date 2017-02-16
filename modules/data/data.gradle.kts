c0ee5bd51827d2f3785e08ad67958ac16af684bd
ingo <ingo.kegel@ej-technologies.com>
2017-02-16 11:57:19 +0100

    Converted to gradle script kotlin

--- modules/data/data.gradle.kts
+++ modules/data/data.gradle.kts
@@ -1,53 +1,74 @@
-apply plugin: 'kotlin'
-apply plugin: 'org.jetbrains.dokka'
+import org.gradle.api.publish.PublishingExtension
+import org.gradle.api.publish.maven.MavenPublication
+import org.gradle.api.tasks.Copy
+import org.gradle.jvm.tasks.Jar
+import org.jetbrains.dokka.gradle.DokkaTask
 
-apply from: file('../publish.gradle')
-
-dependencies {
-    compile "org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion"
-}
-
-apply plugin: 'java'
-
-jar {
-    archiveName = 'jclasslib-library.jar'
-}
-
-dokka {
-    processConfigurations = ["compile"]
-    includes = ['packages.md']
-}
-
-task dokkaJavadoc(type: org.jetbrains.dokka.gradle.DokkaTask) {
-    outputFormat = 'javadoc'
-    outputDirectory = "$buildDir/javadoc"
-}
-
-task javadocJar(type: Jar, dependsOn: dokkaJavadoc) {
-    classifier = 'javadoc'
-    from dokkaJavadoc.outputDirectory
-}
-
-publishing {
-    publications {
-        Module(MavenPublication) {
-            artifact javadocJar {
-                classifier "javadoc"
-            }
-        }
+buildscript {
+    repositories {
+        jcenter()
+    }
+    dependencies {
+        //TODO convert to plugins dependency once that works
+        classpath("org.jetbrains.dokka:dokka-gradle-plugin:0.9.13")
     }
 }
 
-task doc {
-    dependsOn dokka, dokkaJavadoc
+apply {
+    plugin("kotlin")
+    plugin("org.jetbrains.dokka")
+    from(file("../publish.gradle"))
 }
 
-task copyDist(type: Copy, dependsOn: jar) {
-    from configurations.compile
-    from jar.archivePath
-    into externalLibsDir
+
+val kotlinVersion = rootProject.extra["kotlinVersion"]
+dependencies {
+    compile("org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion")
 }
 
-task dist {
-    dependsOn doc, copyDist
+val publications = the<PublishingExtension>().publications
+
+tasks {
+    val jar: Jar by tasks
+    jar.apply {
+        archiveName = "jclasslib-library.jar"
+    }
+
+    val copyDist by creating(Copy::class) {
+        dependsOn("jar")
+        from(configurations.compile)
+        from(jar.archivePath)
+        into(rootProject.extra["externalLibsDir"])
+    }
+
+    val dokka: DokkaTask by tasks
+    dokka.apply {
+        processConfigurations = listOf("compile")
+        includes = listOf("packages.md")
+    }
+
+    val dokkaJavadoc by creating(DokkaTask::class) {
+        outputFormat = "javadoc"
+        outputDirectory = "$buildDir/javadoc"
+    }
+
+    val doc by creating {
+        dependsOn(dokka, dokkaJavadoc)
+    }
+
+    val javadocJar by creating(Jar::class) {
+        dependsOn(dokkaJavadoc)
+        classifier = "javadoc"
+        from(dokkaJavadoc.outputDirectory)
+    }
+
+    publications {
+        "Module"(MavenPublication::class) {
+            artifact(mapOf("source" to javadocJar, "classifier" to "javadoc"))
+        }
+    }
+
+    "dist" {
+        dependsOn(doc, copyDist)
+    }
 }
