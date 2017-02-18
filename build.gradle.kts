1a2c9c97fbf6c85acd9774d5ae0e7c68a48e4946
ingo <ingo.kegel@ej-technologies.com>
2017-02-18 17:43:46 +0100

    Converted to gradle script kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,112 +1,145 @@
-apply plugin: 'idea'
+import org.gradle.api.tasks.compile.JavaCompile
+import org.gradle.api.tasks.testing.Test
+import org.gradle.api.tasks.wrapper.Wrapper
+import org.gradle.plugins.ide.idea.model.IdeaModel
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-version = '5.2'
-ext.mediaDir = file('media')
+apply {
+    plugin("idea")
+}
 
-buildDir = file('build/gradle')
-ext.externalLibsDir = file("$buildDir/externalLibs")
+version = "5.2"
 
-buildscript {scriptHandler ->
-    ext.kotlinVersion = '1.1.3-2'
-    String[] kotlinVersionParts = kotlinVersion.split('-')
-    ext.kotlinVersionMain = kotlinVersionParts[0]
-    ext.kotlinVersionHotfix = kotlinVersionParts.length > 1 ? "-${kotlinVersionParts[1]}" : ''
+setBuildDir(file("build/gradle"))
+
+var mediaDir: File by extra
+mediaDir = file("media")
+
+var externalLibsDir: File by extra
+externalLibsDir = file("$buildDir/externalLibs")
+
+val kotlinVersion: String by extra
+
+buildscript {
+    val kotlinVersion = "1.1.3-2"
+    extra["kotlinVersion"] = kotlinVersion
+    val kotlinVersionParts = kotlinVersion.split('-')
+    extra["kotlinVersionMain"] = kotlinVersionParts[0]
+    extra["kotlinVersionHotfix"] = if (kotlinVersionParts.length > 1) "-${kotlinVersionParts[1]}" else  ""
+
+    val mavenUrls = listOf("http://jcenter.bintray.com", "http://maven.ej-technologies.com/repository", "http://dl.bintray.com/kotlin/kotlin-eap-1.1")
+    extra["mavenUrls"] = mavenUrls
 
     repositories {
         flatDir {
-            dirs 'lib-compile'
+            setDirs(listOf("lib-compile"))
         }
         maven {
-            url 'http://dl.bintray.com/jetbrains/intellij-plugin-service'
+            setUrl("http://dl.bintray.com/jetbrains/intellij-plugin-service")
+        }
+        for (mavenUrl in mavenUrls) {
+            maven {
+                setUrl(mavenUrl)
+            }
         }
     }
-    apply from: file('repositories.gradle'), to: scriptHandler
 
     dependencies {
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion"
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${extra["kotlinVersion"]}")
 
         // for local dokka in lib-compile
         //classpath ':dokka-fatjar'
         //classpath ':dokka-gradle-plugin'
         classpath 'org.jetbrains.dokka:dokka-gradle-plugin:0.9.15'
-        classpath "com.install4j:install4j-gradle:7.0"
-        classpath 'com.jfrog.bintray.gradle:gradle-bintray-plugin:1.7.3'
+        classpath("com.install4j:install4j-gradle:7.0")
+        classpath("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.7.3")
     }
 }
 
-allprojects {
+val mavenUrls: List<String> by extra
 
-    group = 'org.gjt.jclasslib'
-    buildDir = new File(rootProject.buildDir, path.substring(1).replaceAll(':', '/'))
+subprojects {
+
+    setBuildDir(File(rootProject.buildDir, path.substring(1).replace(':', '/')))
+
+    group = "org.gjt.jclasslib"
     version = rootProject.version
 
     repositories {
         flatDir {
-            dirs file('lib'), file("$rootDir/lib-compile")
+            setDirs(listOf(file("lib"), file("$rootDir/lib-compile")))
+        }
+        for (mavenUrl in mavenUrls) {
+            maven {
+                setUrl(mavenUrl)
+            }
         }
     }
-    apply from: file("$rootDir/repositories.gradle")
 
-    plugins.withType(JavaPlugin).whenPluginAdded {
+    plugins.withType<JavaPlugin> {
         dependencies {
-            testCompile 'org.testng:testng:6.8.8'
+            testCompile("org.testng:testng:6.8.8")
         }
 
-        tasks.withType(JavaCompile).each {JavaCompile compileJava ->
-            compileJava.with {
-                sourceCompatibility = '1.8'
-                targetCompatibility = '1.8'
+        tasks.withType<JavaCompile>().forEach {compileJava ->
+            compileJava.apply {
+                sourceCompatibility = "1.8"
+                targetCompatibility = "1.8"
             }
         }
 
-        tasks.withType(Test).each {Test test ->
-            test.with {
+        tasks.withType<Test>().forEach {test ->
+            test.apply {
                 useTestNG()
             }
         }
 
-    }
-    
-    plugins.withId("kotlin") {
-        tasks.getByName("compileKotlin").kotlinOptions.languageVersion = "1.1"
+        tasks.withType<KotlinCompile> {
+            kotlinOptions {
+                languageVersion = "1.1"
+            }
+        }
     }
 }
 
-task clean {
+val clean by tasks.creating {
     doLast {
-        delete externalLibsDir
+        delete(externalLibsDir)
     }
 }
 
-task wrapper(type: Wrapper) {
-    gradleVersion = '4.0.1'
-    distributionType = Wrapper.DistributionType.ALL
-}
+val dist by tasks.creating {}
+val test by tasks.creating {}
 
-task test()
-task dist()
+tasks {
+    "wrapper"(Wrapper::class) {
+        gradleVersion = "4.0.1"
+        distributionType = Wrapper.DistributionType.ALL
+    }
+}
 
 gradle.projectsEvaluated {
-    getTasksByName('clean', true).each {task ->
+    getTasksByName("clean", true).forEach { task ->
         if (task != clean) {
             clean.dependsOn(task)
         }
     }
-    getTasksByName('dist', true).each {task ->
+    getTasksByName("dist", true).forEach { task ->
         if (task != dist) {
             dist.dependsOn(task)
         }
     }
-    getTasksByName('test', true).each {task ->
+    getTasksByName("test", true).forEach { task ->
         if (task != test) {
             test.dependsOn(task)
         }
     }
 }
 
-idea {
+val idea: IdeaModel by extensions
+idea.apply {
     module {
-        name = 'root'
-        excludeDirs += files('build', 'dist', 'media')
+        name = "root"
+        excludeDirs = files("build", "dist", "media").files + excludeDirs
     }
 }
