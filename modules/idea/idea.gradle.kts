c1633bcb16e54ee51dd62c320a4db36d49e1f13d
Ingo Kegel <ingo.kegel@ej-technologies.com>
2018-12-27 14:33:05 +0100

    Gradle IDEA plugin needs a higher version of Apache HttpClient than what is requested by the Bintray plugin in buildSrc

--- modules/idea/idea.gradle.kts
+++ modules/idea/idea.gradle.kts
@@ -1,8 +1,9 @@
 import org.jetbrains.intellij.tasks.PublishTask
+import org.jetbrains.intellij.tasks.RunIdeTask
 
 plugins {
     kotlin("jvm")
-    id("org.jetbrains.intellij") version "0.3.12"
+    id("org.jetbrains.intellij") version "0.4.1"
 }
 
 val kotlinVersion: String by project
@@ -12,16 +13,20 @@ dependencies {
 }
 
 intellij {
-    version = "IC-2017.3.4"
+    version = "IC-2018.3.2"
     pluginName = "jclasslib"
-    setPlugins("ByteCodeViewer", "org.jetbrains.kotlin:$kotlinVersion-release-IJ2017.3-1")
+    setPlugins("ByteCodeViewer", "org.jetbrains.kotlin:$kotlinVersion-release-IJ2018.3-1")
     sandboxDirectory = "${rootProject.buildDir}/../idea_sandbox"
     updateSinceUntilBuild = false
 }
 
 tasks {
-    "publishPlugin"(PublishTask::class) {
+    publishPlugin {
         setUsername(project.findProperty("intellij.publish.username") ?: "")
         setPassword(project.findProperty("intellij.publish.password") ?: "")
+    }
+
+    runIde {
+        jvmArgs("-Xmx1g")
     }
 }
