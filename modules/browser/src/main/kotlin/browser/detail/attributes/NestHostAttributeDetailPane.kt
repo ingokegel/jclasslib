afc0891e4d996c6a9c0a16b1ae53dd7b2527ca83
Ingo Kegel <ingo.kegel@ej-technologies.com>
2020-03-26 15:45:54 +0100

    Cleaned up license headers

--- modules/browser/src/main/kotlin/browser/detail/attributes/NestHostAttributeDetailPane.kt
+++ modules/browser/src/main/kotlin/browser/detail/attributes/NestHostAttributeDetailPane.kt
@@ -5,13 +5,6 @@
  version 2 of the license, or (at your option) any later version.
  */
 
-/*
- This library is free software; you can redistribute it and/or
- modify it under the terms of the GNU General Public
- License as published by the Free Software Foundation; either
- version 2 of the license, or (at your option) any later version.
- */
-
 package org.gjt.jclasslib.browser.detail.attributes
 
 import org.gjt.jclasslib.browser.BrowserServices
