6bebd8577970dc72dee8e9484383411e9abae4e3
Ingo Kegel <ingo.kegel@ej-technologies.com>
2021-07-08 13:04:42 +0200

    install4j 9.0.3

--- modules/installer/installer.gradle.kts
+++ modules/installer/installer.gradle.kts
@@ -1,7 +1,7 @@
 import com.install4j.gradle.Install4jTask
 
 plugins {
-    id("com.install4j.gradle") version "8.0.8"
+    id("com.install4j.gradle") version "9.0.3"
 }
 
 val install4jHomeDir: String? by project
