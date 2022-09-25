a26e157cd1a347c3b39c6384a4d92aaba6407974
Ingo Kegel <ingo.kegel@ej-technologies.com>
2022-09-25 15:08:05 +0200

    FlatLaf 2.4

--- modules/browser/browser.gradle.kts
+++ modules/browser/browser.gradle.kts
@@ -10,7 +10,7 @@ application {
     mainClass.set("org.gjt.jclasslib.browser.BrowserApplication")
 }
 
-val flatLafVersion = "2.3"
+val flatLafVersion = "2.4"
 
 dependencies {
     api(project(":agent"))
