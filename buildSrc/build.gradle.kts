5f2749d55b75dfc3af77fafc9a619061ed238af5
Ingo Kegel <ingo.kegel@ej-technologies.com>
2025-10-16 10:37:48 +0200

    Dokka 2.1.0

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,6 +8,6 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.2.20")
-    implementation("org.jetbrains.dokka:org.jetbrains.dokka.gradle.plugin:2.0.0")
+    implementation("org.jetbrains.dokka:org.jetbrains.dokka.gradle.plugin:2.1.0")
     implementation("com.vanniktech.maven.publish:com.vanniktech.maven.publish.gradle.plugin:0.34.0")
 }
