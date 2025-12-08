a01de75c66c64d577013b14665c4afd739544a66
Ingo Kegel <ingo.kegel@ej-technologies.com>
2025-12-08 14:53:13 +0100

    New app icon, use install4j 12

--- modules/installer/installer.gradle.kts
+++ modules/installer/installer.gradle.kts
@@ -1,7 +1,7 @@
 import com.install4j.gradle.Install4jTask
 
 plugins {
-    id("com.install4j.gradle") version "11.0.5"
+    id("com.install4j.gradle") version "12.0.1"
 }
 
 val winCertPath: String? by project
