0fdd78bfa8fe1df4e80e120ef93d2b9c6edcaa6f
Ingo Kegel <ingo.kegel@ej-technologies.com>
2025-12-19 10:35:52 +0100

    separate tasks for downloadable media files and appstore package

--- modules/installer/installer.gradle.kts
+++ modules/installer/installer.gradle.kts
@@ -6,7 +6,6 @@ plugins {
 
 val macProvisioningProfile: String? by project
 val appStoreCerts: String? by project
-val appStore = project.findProperty("appStoreOnly").toString().toBoolean()
 val appleIssuerId: String? by project
 val appleKeyId: String? by project
 val applePrivateApiKey: String? by project
@@ -18,35 +17,44 @@ tasks {
     }
 
     register<Install4jTask>("media") {
-        dependsOn(":dist")
+        configureInstall4j()
+        buildIds = listOf("2047307151", "2047307321", "2047307325", "850942491")
+    }
 
-        inputs.dir(rootProject.file("modules"))
-        inputs.files(rootProject.file("license"))
-
-        group = "Build"
-        description = "Build all media files"
-
-        projectFile = file("resources/jclasslib.install4j")
-        release = version as String
-        macKeystorePassword = ""
-
-        if (appleIssuerId == null || appleKeyId == null || applePrivateApiKey == null) {
-            disableNotarization = true
-        }
-
-        variables = mapOf(
+    register<Install4jTask>("appstore") {
+        configureInstall4j()
+        buildIds = listOf("2047307322")
+        variables.putAll(mapOf(
             "macProvisioningProfile" to (macProvisioningProfile ?: ""),
             "appStoreCerts" to (appStoreCerts ?: ""),
-            "digestSigningCommandLine" to (digestSigningCommandLine ?: ""),
-            "appleIssuerId" to (appleIssuerId ?: ""),
-            "appleKeyId" to (appleKeyId ?: ""),
-            "applePrivateApiKey" to (applePrivateApiKey ?: ""),
-        ) + if (appStore) mapOf("sys.ext.macKeySource" to "pkcs12") else emptyMap()
-
-        if (appStore) {
-            buildIds = listOf("2047307322")
-        } else {
-            buildIds = listOf("2047307151", "2047307321", "2047307325", "850942491")
-        }
+            "sys.ext.macKeySource" to "pkcs12"
+        ))
     }
+}
+
+
+fun Install4jTask.configureInstall4j() {
+    dependsOn(":dist")
+
+    inputs.dir(rootProject.file("modules"))
+    inputs.files(rootProject.file("license"))
+
+    group = "Build"
+    description = "Build all media files"
+
+    projectFile = file("resources/jclasslib.install4j")
+    release = version as String
+    macKeystorePassword = ""
+
+    if (appleIssuerId == null || appleKeyId == null || applePrivateApiKey == null) {
+        disableNotarization = true
+    }
+
+    variables.putAll(mapOf(
+        "digestSigningCommandLine" to (digestSigningCommandLine ?: ""),
+        "appleIssuerId" to (appleIssuerId ?: ""),
+        "appleKeyId" to (appleKeyId ?: ""),
+        "applePrivateApiKey" to (applePrivateApiKey ?: ""),
+    ))
+
 }
